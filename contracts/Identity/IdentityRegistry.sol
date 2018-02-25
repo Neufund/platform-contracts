20bd4d7de4087f748ee107c73bc2483053dac1e2
Marcin Rudolf <marcin.rudolf@neufund.org>
2018-02-25 10:23:57 +0100

    implements identity registry

--- contracts/Identity/IdentityRegistry.sol
+++ contracts/Identity/IdentityRegistry.sol
@@ -1,10 +1,74 @@
 pragma solidity 0.4.15;
 
+import '../AccessControl/AccessControlled.sol';
+import '../AccessRoles.sol';
+import '../EthereumForkArbiter.sol';
 import "./IIdentityRegistry.sol";
 
 
 contract IdentityRegistry is
-    IIdentityRegistry
+    IIdentityRegistry,
+    AccessControlled,
+    AccessRoles
 {
-    // TODO: imlement
+    ////////////////////////
+    // Immutable state
+    ////////////////////////
+
+    IEthereumForkArbiter private ETHEREUM_FORK_ARBITER;
+
+    ////////////////////////
+    // Mutable state
+    ////////////////////////
+
+    mapping(address => bytes32) private _claims;
+
+    ////////////////////////
+    // Constructor functions
+    ////////////////////////
+
+    function IdentityRegistry(IAccessPolicy accessPolicy, IEthereumForkArbiter forkArbiter)
+        AccessControlled(accessPolicy)
+        public
+    {
+        require(forkArbiter != IEthereumForkArbiter(0x0));
+        ETHEREUM_FORK_ARBITER = forkArbiter;
+    }
+
+    ////////////////////////
+    // Public functions
+    ////////////////////////
+
+    function getClaims(address identity)
+        public
+        constant
+        returns (bytes32 claims)
+    {
+        return _claims[identity];
+    }
+
+    function setClaims(address identity, bytes32 oldClaims, bytes32 newClaims)
+        only(ROLE_IDENTITY_MANAGER)
+        public
+    {
+        require(_claims[identity] == oldClaims);
+        _claims[identity] = newClaims;
+        LogSetClaims(identity, oldClaims, newClaims);
+    }
+
+    /// sets multiple claims in single transaction to save on gas
+    function setMultipleClaims(address[] identities, bytes32[] oldClaims, bytes32[] newClaims)
+        only(ROLE_IDENTITY_MANAGER)
+        public
+    {
+        assert(identities.length == oldClaims.length);
+        assert(identities.length == newClaims.length);
+
+        uint256 idx = identities.length;
+        while(--idx >= 0) {
+            require(_claims[identities[idx]] == oldClaims[idx]);
+            _claims[identities[idx]] = newClaims[idx];
+            LogSetClaims(identities[idx], oldClaims[idx], newClaims[idx]);
+        }
+    }
 }
