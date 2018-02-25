f5e8c836a48a6b8a89ba512074fcd2c4d8f845a3
Marcin Rudolf <marcin.rudolf@neufund.org>
2018-02-25 10:23:37 +0100

    adds banking details to identity record, changes record type to bytes32

--- contracts/Identity/IIdentityRegistry.sol
+++ contracts/Identity/IIdentityRegistry.sol
@@ -1,6 +1,8 @@
 pragma solidity 0.4.15;
 
 
+/// @title describes layout of claims in 256bit records stored for identities
+/// @dev intended to be derived by contracts requiring access to particular claims
 contract IdentityRecord {
 
     ////////////////////////
@@ -13,7 +15,8 @@ contract IdentityRecord {
     struct IdentityClaims {
         bool hasKyc; // 1 bit
         bool isSophisticatedInvestor; // 1 bit
-        // uint254 reserved
+        bool hasBankAccount; // 1 bit
+        // uint253 reserved
     }
 
     ////////////////////////
@@ -21,21 +24,41 @@ contract IdentityRecord {
     ////////////////////////
 
     /// translates uint256 to struct
-    function deserializeClaims(uint256 data) internal constant returns (IdentityClaims memory claims) {
+    function deserializeClaims(bytes32 data) internal constant returns (IdentityClaims memory claims) {
         // for memory layout of struct, each field below word length occupies whole word
         assembly {
             mstore(claims, and(data, 0x1))
-            mstore(add(claims, 0x20), div(and(data, 0x2), 2))
+            mstore(add(claims, 0x20), div(and(data, 0x2), 0x2))
+            mstore(add(claims, 0x40), div(and(data, 0x4), 0x4))
         }
     }
 }
 
 
+/// @title interface storing and retrieve 256bit claims records for identity
+/// actual format of record is decoupled from storage (except maximum size)
 contract IIdentityRegistry {
+
+    ////////////////////////
+    // Events
+    ////////////////////////
+
+    /// provides information on setting claims
+    event LogSetClaims(
+        address indexed identity,
+        bytes32 oldClaims,
+        bytes32 newClaims
+    );
+
+    ////////////////////////
+    // Public functions
+    ////////////////////////
+
     /// get claims for identity
-    function getClaims(address identity) public constant returns (uint256);
+    function getClaims(address identity) public constant returns (bytes32);
+
     /// set claims for identity
     /// @dev odlClaims and newClaims used for optimistic locking. to override with newClaims
     ///     current claims must be oldClaims
-    function setClaims(address identity, uint256 oldClaims, uint256 newClaims) public;
+    function setClaims(address identity, bytes32 oldClaims, bytes32 newClaims) public;
 }
