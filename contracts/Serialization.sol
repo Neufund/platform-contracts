04724482e99cd3a798de2081fa271b0a3b7b014c
Marcin Rudolf <marcin.rudolf@neufund.org>
2018-03-02 15:49:16 +0100

    implements LockedAccount
    closes #15
    closes #16
    closes #17
    closes #19
    closes #45

---
+++ contracts/Serialization.sol
@@ -0,0 +1,35 @@
+pragma solidity 0.4.15;
+
+
+/// @title serialization of basic types from/to bytes
+contract Serialization {
+    ////////////////////////
+    // Internal functions
+    ////////////////////////
+    function addressToBytes(address a)
+        internal
+        constant
+        returns (bytes b)
+    {
+        assembly {
+            let m := mload(0x40)
+            // use unsed part of address representation to add length prefix (20) and then position it
+            // so it pretends to be length prefixed 64 bytes "bytes"
+            mstore(add(m, 20), xor(0x140000000000000000000000000000000000000000, a))
+            mstore(0x40, add(m, 52))
+            b := m
+        }
+    }
+
+    function addressFromBytes(bytes b)
+        internal
+        constant
+        returns (address a)
+    {
+        require(b.length == 20);
+        assembly {
+            // load memory area that is address "carved out" of 64 byte bytes. prefix is zeroed
+            a := and(mload(add(b, 20)), 0x00FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF)
+        }
+    }
+}
