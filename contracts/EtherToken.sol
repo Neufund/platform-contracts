96700b467f5dcfac35dcaa6f5b80bbd4f633ae5a
Marcin Rudolf <marcin.rudolf@neufund.org>
2018-03-12 23:13:12 +0100

    implements new EtherToken with convenience methods
    closes #53

--- contracts/EtherToken.sol
+++ contracts/EtherToken.sol
@@ -41,6 +41,12 @@ contract EtherToken is
         uint256 amount
     );
 
+    event LogWithdrawAndSend(
+        address indexed from,
+        address indexed to,
+        uint256 amount
+    );
+
     ////////////////////////
     // Constructor
     ////////////////////////
@@ -63,22 +69,47 @@ contract EtherToken is
         payable
         public
     {
-        _balances[msg.sender] = add(_balances[msg.sender], msg.value);
-        _totalSupply = add(_totalSupply, msg.value);
-        LogDeposit(msg.sender, msg.value);
+        depositPrivate();
         Transfer(address(0), msg.sender, msg.value);
+    }
+
+    /// @notice convenience function to deposit and immediately transfer amount
+    /// @param transferTo where to transfer after deposit
+    /// @param amount total amount to transfer, must be <= balance after deposit
+    /// @param data erc223 data
+    /// @dev intended to deposit from simple account and invest in ETO
+    function depositAndTransfer(address transferTo, uint256 amount, bytes data)
+        payable
+        public
+    {
+        depositPrivate();
+        transfer(transferTo, amount, data);
     }
 
     /// withdraws and sends 'amount' of ether to msg.sender
     function withdraw(uint256 amount)
         public
     {
-        require(_balances[msg.sender] >= amount);
-        _balances[msg.sender] = sub(_balances[msg.sender], amount);
-        _totalSupply = sub(_totalSupply, amount);
+        withdrawPrivate(amount);
         msg.sender.transfer(amount);
-        LogWithdrawal(msg.sender, amount);
-        Transfer(msg.sender, address(0), amount);
+    }
+
+    /// @notice convenience function to withdraw and transfer to external account
+    /// @param sendTo address to which send total amount
+    /// @param amount total amount to withdraw and send
+    /// @dev function is payable and is meant to withdraw funds on accounts balance and token in single transaction
+    function withdrawAndSend(address sendTo, uint256 amount)
+        payable
+        public
+    {
+        // must send at least what is in msg.value to being another deposit function
+        require(amount >= msg.value);
+        if (amount > msg.value) {
+            uint256 withdrawRemainder = amount - msg.value;
+            withdrawPrivate(withdrawRemainder);
+        }
+        LogWithdrawAndSend(msg.sender, sendTo, amount);
+        sendTo.transfer(amount);
     }
 
     //
@@ -112,5 +143,27 @@ contract EtherToken is
         // forbid reclaiming ETH hold in this contract.
         require(token != RECLAIM_ETHER);
         Reclaimable.reclaim(token);
+    }
+
+    ////////////////////////
+    // Private functions
+    ////////////////////////
+
+    function depositPrivate()
+        private
+    {
+        _balances[msg.sender] = add(_balances[msg.sender], msg.value);
+        _totalSupply = add(_totalSupply, msg.value);
+        LogDeposit(msg.sender, msg.value);
+    }
+
+    function withdrawPrivate(uint256 amount)
+        private
+    {
+        require(_balances[msg.sender] >= amount);
+        _balances[msg.sender] = sub(_balances[msg.sender], amount);
+        _totalSupply = sub(_totalSupply, amount);
+        LogWithdrawal(msg.sender, amount);
+        Transfer(msg.sender, address(0), amount);
     }
 }
