9552d5a2f0c63c2a2630b9f93be565fb148b186e
Marcin Rudolf <marcin.rudolf@neufund.org>
2018-04-10 00:33:07 +0200

    patches icbm state for test deployment so it is post icbm commitment

--- migrations/7_patch_icbm_state.js
+++ migrations/7_patch_icbm_state.js
@@ -1,58 +1,52 @@
 require("babel-register");
 const getConfig = require("./config").getConfig;
-const roles = require("../test/helpers/roles").default;
-// const getDeployerAccount = require("./config").getDeployerAccount;
-
-const { TriState } = require("../test/helpers/triState");
+const getFixtureAccounts = require("./config").getFixtureAccounts;
 
 module.exports = function deployContracts(deployer, network, accounts) {
   const CONFIG = getConfig(web3, network, accounts);
   // skip fixtures on live deployment
-  if (CONFIG.shouldSkipDeployment||CONFIG.isLiveDeployment) return;
-  const RoleBasedAccessPolicy = artifacts.require(CONFIG.artifacts.ROLE_BASED_ACCESS_POLICY);
+  if (CONFIG.shouldSkipDeployment || CONFIG.isLiveDeployment) return;
+  const fas = getFixtureAccounts(accounts);
   const Commitment = artifacts.require(CONFIG.artifacts.ICBM_COMMITMENT);
   const ICBMEuroToken = artifacts.require(CONFIG.artifacts.ICBM_EURO_TOKEN);
-  const Neumark = artifacts.require(CONFIG.artifacts.NEUMARK);
-
-  // const DEPLOYER = getDeployerAccount(network, accounts);
 
   deployer.then(async () => {
     const commitment = await Commitment.deployed();
     const euroToken = await ICBMEuroToken.deployed();
-    const neumark = await Neumark.deployed();
-    const accessPolicy = await RoleBasedAccessPolicy.deployed();
     console.log("set Commitment to public phase");
     await commitment._mockTransitionTo(1);
     await commitment._mockTransitionTo(2);
     console.log("commit ETH");
-    await commitment.commit({ from: accounts[1], value: CONFIG.Q18.mul(61.1289798) });
-    await commitment.commit({ from: accounts[3], value: CONFIG.Q18.mul(61.1289798) });
+    await commitment.commit({
+      from: fas.ICBM_ETH_NOT_MIGRATED_NO_KYC,
+      value: CONFIG.Q18.mul(61.1289798),
+    });
+    await commitment.commit({
+      from: fas.ICBM_EUR_ETH_NOT_MIGRATED_HAS_KYC,
+      value: CONFIG.Q18.mul(18.1289798),
+    });
+    await commitment.commit({
+      from: fas.ICBM_ETH_MIGRATED_NO_KYC,
+      value: CONFIG.Q18.mul(11.1289798),
+    });
     console.log("commit EUR");
     let amountEur = CONFIG.Q18.mul(1781267);
-    await euroToken.deposit(accounts[2], amountEur);
-    await euroToken.approve(commitment.address, amountEur, { from: accounts[2] });
-    await commitment.commitEuro({from: accounts[2]});
+    await euroToken.deposit(fas.ICBM_EUR_NOT_MIGRATED_HAS_KYC, amountEur);
+    await euroToken.approve(commitment.address, amountEur, {
+      from: fas.ICBM_EUR_NOT_MIGRATED_HAS_KYC,
+    });
+    await commitment.commitEuro({ from: fas.ICBM_EUR_NOT_MIGRATED_HAS_KYC });
     amountEur = CONFIG.Q18.mul(71827);
-    await euroToken.deposit(accounts[3], amountEur);
-    await euroToken.approve(commitment.address, amountEur, { from: accounts[3] });
-    await commitment.commitEuro({from: accounts[3]});
+    await euroToken.deposit(fas.ICBM_EUR_ETH_NOT_MIGRATED_HAS_KYC, amountEur);
+    await euroToken.approve(commitment.address, amountEur, {
+      from: fas.ICBM_EUR_ETH_NOT_MIGRATED_HAS_KYC,
+    });
+    await commitment.commitEuro({ from: fas.ICBM_EUR_ETH_NOT_MIGRATED_HAS_KYC });
+    amountEur = CONFIG.Q18.mul(812988.1988);
+    await euroToken.deposit(fas.ICBM_EUR_MIGRATED_HAS_KYC, amountEur);
+    await euroToken.approve(commitment.address, amountEur, { from: fas.ICBM_EUR_MIGRATED_HAS_KYC });
+    await commitment.commitEuro({ from: fas.ICBM_EUR_MIGRATED_HAS_KYC });
     console.log("set Commitment to final phase");
     await commitment._mockTransitionTo(3);
-    // const finalized = await commitment.state();
-    // const agreement = await commitment.currentAgreement();
-    // console.log(agreement);
-    console.log("drop permissions from icbm contracts");
-    await accessPolicy.setUserRole(
-      commitment.address,
-      roles.neumarkIssuer,
-      neumark.address,
-      TriState.Unset,
-    );
-    await accessPolicy.setUserRole(
-      commitment.address,
-      roles.transferAdmin,
-      neumark.address,
-      TriState.Unset,
-    );
   });
 };
