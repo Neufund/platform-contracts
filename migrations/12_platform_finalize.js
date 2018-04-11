71784dbaa82650b795184fffff1337d8e0423c08
Marcin Rudolf <marcin.rudolf@neufund.org>
2018-04-11 14:47:04 +0200

    stores deploy metadata in build folder

--- migrations/12_platform_finalize.js
+++ migrations/12_platform_finalize.js
@@ -1,7 +1,12 @@
 require("babel-register");
+const fs = require("fs");
+const { join } = require("path");
 const getConfig = require("./config").getConfig;
 const getDeployerAccount = require("./config").getDeployerAccount;
+const getNetworkDefinition = require("./config").getNetworkDefinition;
 const roles = require("../test/helpers/roles").default;
+const knownInterfaces = require("../test/helpers/knownInterfaces").default;
+const interfaceArtifacts = require("../test/helpers/interfaceArtifacts").default;
 const { TriState } = require("../test/helpers/triState");
 const createAccessPolicy = require("../test/helpers/createAccessPolicy").default;
 const getFixtureAccounts = require("./config").getFixtureAccounts;
@@ -34,10 +39,25 @@ module.exports = function deployContracts(deployer, network, accounts) {
       console.log("---------------------------------------------");
     }
 
+    const meta = {
+      CONFIG,
+      DEPLOYER,
+      UNIVERSE_ADDRESS: universe.address,
+      ROLES: roles,
+      KNOWN_INTERFACES: knownInterfaces,
+      INTERFACE_ARTIFACTS: interfaceArtifacts,
+      NETWORK: getNetworkDefinition(network),
+      FIXTURE_ACCOUNTS: fas,
+    };
+    const path = join(__dirname, "../build/meta.json");
+    fs.writeFile(path, JSON.stringify(meta), err => {
+        if (err) throw new Error(err)
+      }
+    );
+
     console.log("---------------------------------------------");
     console.log(`Universe is ${universe.address}`);
-    console.log(`Deployment artifacts are in ${network}`);
-    console.log(`Accounts with fixtures: ${fas}`);
+    console.log(`Deployment artifacts are in "${path}"`);
     console.log("---------------------------------------------");
   });
 };
