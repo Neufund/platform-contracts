9db29a97aa2c5c0c5634ac1bb900843fafa1cebf
Marcin Rudolf <marcin.rudolf@neufund.org>
2018-04-11 14:46:36 +0200

    adds script to deploy and store artifacts + meta

---
+++ scripts/deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+set -u
+
+# get network so we can provide deploy artifacts correctly
+NETWORK=$1
+if [ -z "$NETWORK" ]; then
+  echo "provide deploy network name as first positional argument"
+  echo "  ie. ./deploy.sh nf_dev"
+  exit -1
+fi
+
+git submodule update --recursive --remote
+if [ ! -d ./platform-contracts-artifacts/build ]; then
+  echo "please provide build artifacts in ./platform-contracts-artifacts/build"
+fi
+rm -r ./build
+cp -r ./platform-contracts-artifacts/build ./build
+yarn truffle deploy --reset --network $NETWORK
+# copy deployed artifacts
+if [ -d ./platform-contracts-artifacts/$NETWORK ]; then
+  rm -r ./platform-contracts-artifacts/$NETWORK
+fi
+cp -r ./build ./platform-contracts-artifacts/$NETWORK
