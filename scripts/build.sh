ee5f0056824e6c9e75869402040f51541dbfd204
Marcin Rudolf <marcin.rudolf@neufund.org>
2018-04-13 17:34:34 +0200

    conditionally remove ./build

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,14 +3,14 @@
 set -e
 set -u
 
-rm -rf ./build && truffle compile --all
+if [ -d ./build ]; then
+  rm -rf ./build
+fi
+truffle compile --all
 if [ ! -d ./platform-contracts-artifacts ]; then
   git clone https://github.com/Neufund/platform-contracts-artifacts.git
 else
   cd ./platform-contracts-artifacts && git reset --hard origin/master && git pull && cd ..
-fi
-if [ -d ./build ]; then
-  rm -r ./build
 fi
 if [ -d ./platform-contracts-artifacts/build ]; then
   rm -r ./platform-contracts-artifacts/build
