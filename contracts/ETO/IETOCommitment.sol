1a5bbdf7f4c125c41ad4a9ea5ce9abac7437de12
Marcin Rudolf <marcin.rudolf@neufund.org>
2018-05-06 17:42:30 +0200

    declares methods in IETOCommitment
    investor methods and offer information getters

--- contracts/ETO/IETOCommitment.sol
+++ contracts/ETO/IETOCommitment.sol
@@ -1,6 +1,8 @@
 pragma solidity 0.4.23;
 
 import "./ICommitment.sol";
+import "./ETOTerms.sol";
+import "../Company/IEquityToken.sol";
 
 
 /// @title default interface of commitment process
@@ -12,13 +14,13 @@ contract IETOCommitment is ICommitment {
 
     // order must reflect time precedence, do not change order below
     enum State {
-        Setup,
+        Setup, // Initial state
         Whitelist,
         Public,
-        Refund,
         Signing,
         Claim,
-        Payout
+        Payout, // Terminal state
+        Refund // Terminal state
     }
 
     ////////////////////////
@@ -32,20 +34,18 @@ contract IETOCommitment is ICommitment {
     );
 
     /// on a claim by invester
-    ///   `investor` claimed `amount` of `assetToken` obtained for
-    ///   `eurEquivalent` and claimed `nmkReward` amount of NEU
-    event LogClaimed(
+    ///   `investor` claimed `amount` of `assetToken` and claimed `nmkReward` amount of NEU
+    event LogTokensClaimed(
         address indexed investor,
         address indexed assetToken,
         uint256 amount,
-        uint256 eurEquivalent,
         uint256 nmkReward
     );
 
     /// on a refund to investor
     ///   `investor` was refunded `amount` of `paymentToken`
     /// @dev may be raised multiple times per refund operation
-    event LogReund(
+    event LogFundsRefunded(
         address indexed investor,
         address indexed paymentToken,
         uint256 amount
@@ -56,17 +56,60 @@ contract IETOCommitment is ICommitment {
     // Public functions
     ////////////////////////
 
-    // state control
+    //
+    // State control
+    //
 
     // returns current ETO state
     function state() public constant returns (State);
 
-    // process control
+    // returns start of given state
+    function startOf(State s) public constant returns (uint256);
 
-    /// refunds investor if ETO is a fail
+    //
+    // Commitment process
+    //
+
+    /// refunds investor if ETO failed
     function refund() external;
+
+    function refundMany(address[] investors) external;
 
     /// claims tokens if ETO is a success
     function claim() external;
+
+    function claimMany(address[] investors) external;
+
+    // initiate fees payout
+    function payout() external;
+
+
+    //
+    // Offering terms
+    //
+
+    function etoTerms() public constant returns (ETOTerms);
+
+    function platformTerms() public constant returns (ETOPlatformTerms);
+
+    // equity token
+    function equityToken() public constant returns (IEquityToken);
+
+    // url to prospectus
+    function prospectusUrl() public constant returns (string);
+
+    /// signed agreement as provided by company and nominee
+    /// @dev available on Claim state transition
+    function signedInvestmentAgreementUrl() public constant returns (string);
+
+    /// financial outcome of token offering set on Signing state transition
+    /// @dev in preceding states 0 is returned
+    function signedOfferingResults()
+    public
+    constant
+    returns (
+        uint256 newShares, uint256 capitalIncreaseEurUlps,
+        uint256 additionalContributionEth, uint256 additionalContributionEurUlps,
+        uint256 tokenParticipationFeeInt, uint256 platformFeeEth, uint256 platformFeeEurUlps);
 
 }
