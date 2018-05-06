45c3110b6ede35df74ba5aabc16eec25b8238583
Marcin Rudolf <marcin.rudolf@neufund.org>
2018-05-06 17:39:43 +0200

    documents possible shareholder rights in ShareholderRights as per legal agreements
    ISHA + THA

--- contracts/Company/ShareholderRights.sol
+++ contracts/Company/ShareholderRights.sol
@@ -2,5 +2,17 @@ pragma solidity 0.4.23;
 
 
 contract ShareholderRights {
-    // struct
+    //Drag Along Right
+    //General Information Right
+    //Voting Right
+    //Nominee Voting Rule
+    //Nominee Tag Along Voting Rule
+    //Tag Along Right
+    //Liquidation Preference
+    // {gen-general-resolutions-voting-duration-days}
+    // Right of First Refusal
+    // {gen-liquidation-preference-muliplier}
+    // Founders-vesting
+    // token transfers allowed
+
 }
