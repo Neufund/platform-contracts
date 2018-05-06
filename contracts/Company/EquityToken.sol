597a14474b123210675639d79b361dfb2cbc16a6
Marcin Rudolf <marcin.rudolf@neufund.org>
2018-05-06 17:40:34 +0200

    declares IEquityToken interface as per THA legal agreement

--- contracts/Company/EquityToken.sol
+++ contracts/Company/EquityToken.sol
@@ -2,18 +2,26 @@ pragma solidity 0.4.23;
 
 
 import "./IEquityToken.sol";
+import "../Snapshot/DailyAndSnapshotable.sol";
+import "../SnapshotToken/Helpers/TokenMetadata.sol";
 import "../SnapshotToken/StandardSnapshotToken.sol";
 
 
 contract EquityToken is
-    StandardSnapshotToken
-    // IEquityToken
+IEquityToken,
+    // StandardSnapshotToken,
+DailyAndSnapshotable,
+TokenMetadata
 {
     // TODO: implement
     //Company contract is token controller
+    //Decimals is 0! tokens are not divisible but token per share
 
     //enable/disable trading
     //close token (liquidate)
     //whitelist distribution address
-    //move to other managing contract
+    //change token controller
+
+    // transfers to 0 must be blocked
+    // should we have way to recover balance?
 }
--- contracts/Company/IEquityToken.sol
+++ contracts/Company/IEquityToken.sol
@@ -2,13 +2,44 @@ pragma solidity 0.4.23;
 
 import "../Standards/IERC677Token.sol";
 import "../Standards/IERC223Token.sol";
-import "../Standards/ITokenSnapshots.sol";
+import "../Standards/IClonedTokenParent.sol";
+import "./IEquityTokenController.sol";
 
 
 contract IEquityToken is
-        IERC677Token,
         IERC223Token,
-        ITokenSnapshots
+IClonedTokenParent
 {
+    /// @dev equity token is not divisible (Decimals == 0) but single share is represented by
+    ///  tokensPerShare tokens
+    function tokensPerShare() public constant returns (uint256);
 
+    /// nominal value of a share in EUR decimal(18) precision
+    function shareNominalValueEurUlps() public constant returns (uint256);
+
+    /// @notice returns associated token controller
+    function equityTokenController() public constant returns (IEquityTokenController);
+
+    /// @dev only by previous token controller
+    function changeEquityTokenController(address newController) public;
+
+    /// returns current nominee which is contract legal rep
+    function nominee() public constant returns (address);
+
+    /// only by previous nominee
+    function changeNominee(address newNominee) public;
+
+    /// controlled, always issues to self
+    function issueTokens(uint256 amount) public;
+
+    /// controlled, may send tokens even when transfer are disabled: to active ETO only
+    function distributeTokens(address to, uint256 amount) public;
+
+    // controlled, msg.sender is typically failed ETO
+    function destroyTokens(uint256 amount) public;
+
+    /// controlled, irreversibly blocks transferable rights
+    function closeToken() public;
+
+    function isClosedStopped() public constant returns (bool);
 }
