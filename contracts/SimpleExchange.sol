ff8c7de240dc4477ae45ff2c33f51e7025dc5b5b
Marcin Rudolf <marcin.rudolf@neufund.org>
2018-05-25 20:09:19 +0200

    adds storing of any rates between two token instances for SimpleExchange
    also implements missing interface methods and refactors storage

--- contracts/SimpleExchange.sol
+++ contracts/SimpleExchange.sol
@@ -15,6 +15,17 @@ contract SimpleExchange is
     Math
 {
     ////////////////////////
+    // Data types
+    ////////////////////////
+
+    struct TokenRate {
+        // rate of numerator token to denominator token
+        uint128 rateFraction;
+        // timestamp of where rate was updated
+        uint128 timestamp;
+    }
+
+    ////////////////////////
     // Events
     ////////////////////////
 
@@ -23,7 +34,7 @@ contract SimpleExchange is
     event LogGasExchange(
         address indexed gasRecipient,
         uint256 amountEurUlps,
-        uint256 exchangeFeeEurUlps,
+        uint256 exchangeFeeFrac,
         uint256 amountWei,
         uint256 rate
     );
@@ -32,6 +43,12 @@ contract SimpleExchange is
         address indexed numeratorToken,
         address indexed denominatorToken,
         uint256 rate
+    );
+
+    event LogReceivedEther(
+        address sender,
+        uint256 amount,
+        uint256 balance
     );
 
     ////////////////////////
@@ -43,18 +60,21 @@ contract SimpleExchange is
     // euro token to store and transfer euro
     IERC223Token private EURO_TOKEN;
     // where to send euro token
-    address private PLATFORM_WALLET;
+    // address private PLATFORM_WALLET;
 
     ////////////////////////
     // Mutable state
     ////////////////////////
 
+    // rate from numerator to denominator
+    mapping (address => mapping (address => TokenRate)) private _rates;
+
     // Euro to Ether rate decimal fraction
-    uint112 private _eurEthRateFraction;
+    // uint112 private _eurEthRateFraction;
     // Ether to Euro rate decimal fraction
-    uint112 private _ethEurRateFraction;
+    // uint112 private _ethEurRateFraction;
     // block timestamp of last update
-    uint32 private _rateLastUpdatedTimestamp;
+    // uint32 private _rateLastUpdatedTimestamp;
 
     ////////////////////////
     // Constructor
@@ -62,49 +82,54 @@ contract SimpleExchange is
 
     constructor(
         IAccessPolicy accessPolicy,
-        IERC223Token numeratorToken,
-        IERC223Token denominatorToken
+        IERC223Token euroToken,
+        IERC223Token etherToken
     )
         AccessControlled(accessPolicy)
         Reclaimable()
         public
     {
-        require(denominatorToken.decimals() == numeratorToken.decimals());
-        EURO_TOKEN = numeratorToken;
-        ETHER_TOKEN = denominatorToken;
+        EURO_TOKEN = euroToken;
+        ETHER_TOKEN = etherToken;
     }
 
     ////////////////////////
     // Public methods
     ////////////////////////
 
-    /// gas
-    function gasExchange(address gasRecipient, uint256 amountEurUlps, uint256 exchangeFeeEurUlps)
+    /// @notice will exchange amountEurUlps of gasRecipient balance into ether
+    /// @dev EuroTokenController has permanent allowance for gasExchange contract to make such exchange possible when gasRecipient has no Ether
+    ///     (chicken and egg problem is solved). The rate from token rate oracle will be used
+    ///     exchangeFeeFraction will be deduced before the exchange happens
+    function gasExchange(address gasRecipient, uint256 amountEurUlps, uint256 exchangeFeeFraction)
         public
         only(ROLE_GAS_EXCHANGE)
     {
-        var (rate, rateTimestamp) = getExchangeRate(EURO_TOKEN, ETHER_TOKEN);
-        // require if rate older than 4 hours
-        require(block.timestamp - rateTimestamp < 6 hours);
-        gasExchangePrivate(gasRecipient, amountEurUlps, exchangeFeeEurUlps, rate);
+        // fee must be less than 100%
+        assert(exchangeFeeFraction < 10**18);
+        var (rate, rateTimestamp) = getExchangeRatePrivate(EURO_TOKEN, ETHER_TOKEN);
+        // require if rate older than 1 hours
+        require(block.timestamp - rateTimestamp < 1 hours);
+        gasExchangePrivate(gasRecipient, amountEurUlps, exchangeFeeFraction, rate);
     }
 
     function gasExchangeMultiple(
         address[] gasRecipients,
         uint256[] amountsEurUlps,
-        uint256[] exchangeFeesEurUlps
+        uint256 exchangeFeeFraction
     )
         public
         only(ROLE_GAS_EXCHANGE)
     {
+        // fee must be less than 100%
+        assert(exchangeFeeFraction < 10**18);
         require(gasRecipients.length == amountsEurUlps.length);
-        require(gasRecipients.length == exchangeFeesEurUlps.length);
-        var (rate, rateTimestamp) = getExchangeRate(EURO_TOKEN, ETHER_TOKEN);
-        // require if rate older than 4 hours
-        require(block.timestamp - rateTimestamp < 6 hours);
+        var (rate, rateTimestamp) = getExchangeRatePrivate(EURO_TOKEN, ETHER_TOKEN);
+        // require if rate older than 1 hours
+        require(block.timestamp - rateTimestamp < 1 hours);
         uint256 idx;
         while(idx < gasRecipients.length) {
-            gasExchangePrivate(gasRecipients[idx], amountsEurUlps[idx], exchangeFeesEurUlps[idx], rate);
+            gasExchangePrivate(gasRecipients[idx], amountsEurUlps[idx], exchangeFeeFraction, rate);
             idx += 1;
         }
     }
@@ -117,17 +142,22 @@ contract SimpleExchange is
     /// Gnosis does not seem to be working
     /// it seems that for Neufund investor it's best to trust Platform Operator to provide correct information, Platform is aligned via NEU and has no incentive to lie
     /// SimpleExchange is replaceable via Universe. when proper oracle is available we'll move to it
-    function setExchangeRate(address numeratorToken, address denominatorToken, uint112 rateFraction)
+    function setExchangeRate(IERC223Token numeratorToken, IERC223Token denominatorToken, uint256 rateFraction)
         public
         only(ROLE_TOKEN_RATE_ORACLE)
     {
-        require(numeratorToken == address(EURO_TOKEN) && denominatorToken == address(ETHER_TOKEN));
-        // TODO: protect against outliers
-        _eurEthRateFraction = rateFraction;
-        _ethEurRateFraction = 10**18 / rateFraction;
-        _rateLastUpdatedTimestamp = uint32(block.timestamp);
-        emit LogSetExchangeRate(numeratorToken, denominatorToken, rateFraction);
-        emit LogSetExchangeRate(denominatorToken, numeratorToken, _ethEurRateFraction);
+        setExchangeRatePrivate(numeratorToken, denominatorToken, rateFraction);
+    }
+
+    function setExchangeRates(IERC223Token[] numeratorTokens, IERC223Token[] denominatorTokens, uint256[] rateFractions)
+        public
+        only(ROLE_TOKEN_RATE_ORACLE)
+    {
+        require(numeratorTokens.length == denominatorTokens.length);
+        require(numeratorTokens.length == rateFractions.length);
+        for(uint256 idx = 0; idx < numeratorTokens.length; idx++) {
+            setExchangeRatePrivate(numeratorTokens[idx], denominatorTokens[idx], rateFractions[idx]);
+        }
     }
 
     //
@@ -139,15 +169,32 @@ contract SimpleExchange is
         constant
         returns (uint256 rateFraction, uint256 timestamp)
     {
-        require(_rateLastUpdatedTimestamp > 0);
-        if (numeratorToken == address(EURO_TOKEN) && denominatorToken == address(ETHER_TOKEN)) {
-            return (_eurEthRateFraction, _rateLastUpdatedTimestamp);
+        return getExchangeRatePrivate(numeratorToken, denominatorToken);
+    }
+
+    function getExchangeRates(address[] numeratorTokens, address[] denominatorTokens)
+        public
+        constant
+        returns (uint256[] rateFractions, uint256[] timestamps)
+    {
+        require(numeratorTokens.length == denominatorTokens.length);
+        uint256 idx;
+        rateFractions = new uint256[](numeratorTokens.length);
+        timestamps = new uint256[](denominatorTokens.length);
+        while(idx < numeratorTokens.length) {
+            var(rate, timestamp) = getExchangeRatePrivate(numeratorTokens[idx], denominatorTokens[idx]);
+            rateFractions[idx] = rate;
+            timestamps[idx] = timestamp;
+            idx += 1;
         }
-        if (numeratorToken == address(ETHER_TOKEN) && denominatorToken == address(EURO_TOKEN)) {
-            return (_ethEurRateFraction, _rateLastUpdatedTimestamp);
-        }
-        // pair not supported
-        revert();
+    }
+
+    //
+    // Override default function
+    //
+
+    function () external payable {
+        emit LogReceivedEther(msg.sender, msg.value, address(this).balance);
     }
 
     ////////////////////////
@@ -157,17 +204,55 @@ contract SimpleExchange is
     function gasExchangePrivate(
         address gasRecipient,
         uint256 amountEurUlps,
-        uint256 exchangeFeeEurUlps,
+        uint256 exchangeFeeFraction,
         uint256 rate
     )
         private
     {
-        // exchange declared amount
-        uint256 amountEthWei = decimalFraction(amountEurUlps, rate);
-        // transfer out amount + fee
-        assert(EURO_TOKEN.transferFrom(gasRecipient, PLATFORM_WALLET, amountEurUlps + exchangeFeeEurUlps));
+        // exchange declared amount - the exchange fee, no overflow, fee < 0
+        uint256 amountEthWei = decimalFraction(amountEurUlps - decimalFraction(amountEurUlps, exchangeFeeFraction), rate);
+        // take all euro tokens
+        assert(EURO_TOKEN.transferFrom(gasRecipient, this, amountEurUlps));
         // transfer ether to gasRecipient
         gasRecipient.transfer(amountEthWei);
-        emit LogGasExchange(gasRecipient, amountEurUlps, exchangeFeeEurUlps, amountEthWei, rate);
+
+        emit LogGasExchange(gasRecipient, amountEurUlps, exchangeFeeFraction, amountEthWei, rate);
+    }
+
+    function getExchangeRatePrivate(address numeratorToken, address denominatorToken)
+        private
+        constant
+        returns (uint256 rateFraction, uint256 timestamp)
+    {
+        TokenRate storage rate = _rates[numeratorToken][denominatorToken];
+        require(rate.timestamp > 0, "SEX_NO_RATE_INFO");
+        return (rate.rateFraction, rate.timestamp);
+    }
+
+    function setExchangeRatePrivate(
+        IERC223Token numeratorToken,
+        IERC223Token denominatorToken,
+        uint256 rateFraction
+    )
+        private
+    {
+        assert(rateFraction > 0);
+        assert(rateFraction < 2**128);
+        uint256 invRateFraction = proportion(10**18, 10**18, rateFraction);
+        require(invRateFraction < 2**128, "SEX_OVR_INV");
+        require(denominatorToken.decimals() == numeratorToken.decimals(), "SEX_DECIMALS");
+        // TODO: protect against outliers
+        _rates[numeratorToken][denominatorToken] = TokenRate({
+            rateFraction: uint128(rateFraction),
+            timestamp: uint128(block.timestamp)
+        });
+        // also store the invesrse
+        _rates[denominatorToken][numeratorToken] = TokenRate({
+            rateFraction: uint128(invRateFraction),
+            timestamp: uint128(block.timestamp)
+        });
+
+        emit LogSetExchangeRate(numeratorToken, denominatorToken, rateFraction);
+        emit LogSetExchangeRate(denominatorToken, numeratorToken, invRateFraction);
     }
 }
