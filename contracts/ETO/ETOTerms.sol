13daa586de1e676cd4f2e40d83638e0e81346e05
Marcin Rudolf <marcin.rudolf@neufund.org>
2018-05-26 17:04:09 +0200

    adds whitelist and validation to ETOTerms

--- contracts/ETO/ETOTerms.sol
+++ contracts/ETO/ETOTerms.sol
@@ -7,8 +7,27 @@ import "../Math.sol";
 
 
 /// @title base terms of Equity Token Offering
-/// implements simple constant price curve without any discounts
+/// encapsulates pricing, discounts and whitelisting mechanism
+/// @dev to be split is mixins
 contract ETOTerms is Math {
+
+    ////////////////////////
+    // Types
+    ////////////////////////
+
+    // @notice whitelist entry with a discount
+    struct WhitelistTicket {
+        // this also overrides maximum ticket
+        uint128 discountEurUlps;
+        // fixed discount of base price, cannot be 0
+        uint128 fixedDiscountFrac;
+    }
+
+    ////////////////////////
+    // Constants state
+    ////////////////////////
+
+    bytes32 private constant EMPTY_STRING_HASH = keccak256("");
 
     ////////////////////////
     // Immutable state
@@ -55,6 +74,88 @@ contract ETOTerms is Math {
     string public EQUITY_TOKEN_NAME;
     string public EQUITY_TOKEN_SYMBOL;
 
+    // manages whitelist
+    address private WHITELIST_MANAGER;
+
+    ////////////////////////
+    // Mutable state
+    ////////////////////////
+
+    // mapping of investors allowed in whitelist
+    mapping (address => WhitelistTicket) private _whitelist;
+
+    ////////////////////////
+    // Modifiers
+    ////////////////////////
+
+    modifier onlyWhitelistManager() {
+        require(msg.sender == WHITELIST_MANAGER);
+        _;
+    }
+
+    ////////////////////////
+    // Events
+    ////////////////////////
+
+    // raised on invesor added to whitelist
+    event LogInvestorWhitelisted(
+        address indexed investor,
+        uint256 discountEurUlps,
+        uint256 fixedDiscountFrac
+    );
+
+    ////////////////////////
+    // Constructor
+    ////////////////////////
+
+    constructor(
+        ETODurationTerms durationTerms,
+        uint256 existingCompanyShares,
+        uint256 minNumberOfTokens,
+        uint256 maxNumberOfTokens,
+        uint256 tokenEurPriceUlps,
+        uint256 minTicketEurUlps,
+        uint256 maxTicketEurUlps,
+        bool enableTransfersOnSuccess,
+        bool isCrowdfunding,
+        string investmentAgreementUrl,
+        string prospectusUrl,
+        ShareholderRights shareHolderRights,
+        string equityTokenName,
+        string equityTokenSymbol,
+        uint256 shareNominalValueEurUlps
+    )
+        public
+    {
+        require(durationTerms != address(0));
+        require(existingCompanyShares > 0);
+        require(keccak256(abi.encodePacked(prospectusUrl)) != EMPTY_STRING_HASH);
+        require(keccak256(abi.encodePacked(investmentAgreementUrl)) != EMPTY_STRING_HASH);
+        require(keccak256(abi.encodePacked(equityTokenName)) != EMPTY_STRING_HASH);
+        require(keccak256(abi.encodePacked(equityTokenSymbol)) != EMPTY_STRING_HASH);
+        require(shareHolderRights != address(0));
+        // test interface
+        require(shareHolderRights.HAS_GENERAL_INFORMATION_RIGHTS());
+        require(maxNumberOfTokens >= minNumberOfTokens);
+        require(shareNominalValueEurUlps > 0);
+
+        DURATION_TERMS = durationTerms;
+        EXISTING_COMPANY_SHARES = existingCompanyShares;
+        MIN_NUMBER_OF_TOKENS = minNumberOfTokens;
+        MAX_NUMBER_OF_TOKENS = maxNumberOfTokens;
+        TOKEN_PRICE_EUR_ULPS = tokenEurPriceUlps;
+        MIN_TICKET_EUR_ULPS = minTicketEurUlps;
+        MAX_TICKET_EUR_ULPS = maxTicketEurUlps;
+        ENABLE_TRANSFERS_ON_SUCCESS = enableTransfersOnSuccess;
+        IS_CROWDFUNDING = isCrowdfunding;
+        INVESTMENT_AGREEMENT_TEMPLATE_URL = investmentAgreementUrl;
+        PROSPECTUS_URL = prospectusUrl;
+        SHAREHOLDER_RIGHTS = shareHolderRights;
+        EQUITY_TOKEN_NAME = equityTokenName;
+        EQUITY_TOKEN_SYMBOL = equityTokenSymbol;
+        SHARE_NOMINAL_VALUE_EUR_ULPS = shareNominalValueEurUlps;
+        WHITELIST_MANAGER = msg.sender;
+    }
 
     ////////////////////////
     // Public methods
@@ -82,78 +183,123 @@ contract ETOTerms is Math {
         return mul(tokenAmountInt, TOKEN_PRICE_EUR_ULPS);
     }
 
-    // gets company valuation
-    /*function COMPANY_VALUATION_EUR_ULPS() public constant returns(uint256) {
-        return calculateEurUlpsAmount(0, mul(EXISTING_COMPANY_SHARES, TOKENS_PER_SHARE));
-    }*/
-
     // get mincap in EUR
-    function MIN_CAP_EUR_ULPS() public constant returns(uint256) {
+    function ESTIMATED_MIN_CAP_EUR_ULPS() public constant returns(uint256) {
         return calculateEurUlpsAmount(0, MIN_NUMBER_OF_TOKENS);
     }
 
     // get max cap in EUR
-    function MAX_CAP_EUR_ULPS() public constant returns(uint256) {
+    function ESTIMATED_MAX_CAP_EUR_ULPS() public constant returns(uint256) {
         return calculateEurUlpsAmount(0, MAX_NUMBER_OF_TOKENS);
     }
 
-    // gets number of shares for an amount of tokens
-    // function calculateNumberOfShares(uint256 )
-
-    ////////////////////////
-    // Constructor
-    ////////////////////////
-
-    constructor(
-        ETODurationTerms durationTerms,
-        uint256 totalCompanyShares,
-        uint256 minNumberOfTokens,
-        uint256 maxNumberOfTokens,
-        uint256 tokenEurPriceUlps,
-        uint256 minTicketEurUlps,
-        uint256 maxTicketEurUlps,
-        uint256 maxTicketSimpleEurUlps,
-        bool enableTransfersOnSuccess,
-        bool isCrowdfunding,
-        string investmentAgreementUrl,
-        string prospectusUrl,
-        ShareholderRights shareHolderRights,
-        string equityTokenName,
-        string equityTokenSymbol,
-        uint256 shareNominalValueEurUlps
+    function addWhitelisted(
+        address[] investors,
+        uint256[] discountAmountsEurUlps,
+        uint256[] discountsFrac
     )
-        public
+        external
+        onlyWhitelistManager
     {
-        DURATION_TERMS = durationTerms;
-        EXISTING_COMPANY_SHARES = totalCompanyShares;
-        MIN_NUMBER_OF_TOKENS = minNumberOfTokens;
-        MAX_NUMBER_OF_TOKENS = maxNumberOfTokens;
-        TOKEN_PRICE_EUR_ULPS = tokenEurPriceUlps;
-        MIN_TICKET_EUR_ULPS = minTicketEurUlps;
-        MAX_TICKET_EUR_ULPS = maxTicketEurUlps;
-        MAX_TICKET_SIMPLE_EUR_ULPS = maxTicketSimpleEurUlps;
-        ENABLE_TRANSFERS_ON_SUCCESS = enableTransfersOnSuccess;
-        IS_CROWDFUNDING = isCrowdfunding;
-        INVESTMENT_AGREEMENT_TEMPLATE_URL = investmentAgreementUrl;
-        PROSPECTUS_URL = prospectusUrl;
-        SHAREHOLDER_RIGHTS = shareHolderRights;
-        EQUITY_TOKEN_NAME = equityTokenName;
-        EQUITY_TOKEN_SYMBOL = equityTokenSymbol;
-        SHARE_NOMINAL_VALUE_EUR_ULPS = shareNominalValueEurUlps;
+        require(investors.length == discountAmountsEurUlps.length);
+        require(investors.length == discountsFrac.length);
+
+        for (uint256 i = 0; i < investors.length; ++i) {
+            addWhitelistInvestorPrivate(investors[i], discountAmountsEurUlps[i], discountsFrac[i]);
+        }
     }
 
-    ////////////////////////
-    // Public methods
-    ////////////////////////
+    // calculate contribution of investor
+    function calculateContribution(
+        address investor,
+        uint256 totalContributedEurUlps,
+        uint256 existingInvestorContributionEurUlps,
+        uint256 newInvestorContributionEurUlps
+    )
+        public
+        constant
+        returns (
+            bool isWhitelisted,
+            uint256 minTicketEurUlps,
+            uint256 maxTicketEurUlps,
+            uint256 equityTokenInt
+            )
+    {
+        WhitelistTicket storage wlTicket = _whitelist[investor];
+        isWhitelisted = wlTicket.fixedDiscountFrac > 0;
+        minTicketEurUlps = MIN_TICKET_EUR_ULPS;
+        maxTicketEurUlps = max(wlTicket.discountEurUlps, MAX_TICKET_EUR_ULPS);
+        // check if has access to discount
+        uint256 discountedAmount;
+        if (existingInvestorContributionEurUlps < wlTicket.discountEurUlps) {
+            discountedAmount = min(newInvestorContributionEurUlps, wlTicket.discountEurUlps - existingInvestorContributionEurUlps);
+            // discount is fixed so use base token price
+            equityTokenInt = proportion(TOKEN_PRICE_EUR_ULPS, wlTicket.fixedDiscountFrac, discountedAmount);
+        }
+        // if any amount above discount
+        uint remainingAmount = newInvestorContributionEurUlps - discountedAmount;
+        if (remainingAmount > 0) {
+            // use pricing along the curve
+            equityTokenInt += calculateTokenAmount(totalContributedEurUlps + discountedAmount, remainingAmount);
+        }
+    }
+
 
     /// @notice checks terms against platform terms, reverts on invalid
     function requireValidTerms(ETOPlatformTerms platformTerms)
         public
         constant
     {
-        // TODO: write checks
-        // TODO: compute ticket size per regulations and settings in platform terms
-        // TODO: validate ETODurationTerms
+        require(MIN_TICKET_EUR_ULPS >= platformTerms.MIN_TICKET_EUR_ULPS(), "ETO_TERMS_MIN_TICKET_EUR_ULPS");
+        if (IS_CROWDFUNDING) {
+            // additional checks for caps
+            require(ESTIMATED_MAX_CAP_EUR_ULPS() <= platformTerms.MAX_TOTAL_AMOUNT_CROWDFUNDING_EUR_ULPS(), "ETO_TERMS_CROWDF_TOTAL");
+            require(MAX_TICKET_EUR_ULPS <= platformTerms.MAX_TICKET_CROWFUNDING_SOPHISTICATED_EUR_ULPS(), "ETO_TERMS_MAX_TICKET_EUR_ULPS");
+            require(MAX_TICKET_SIMPLE_EUR_ULPS <= platformTerms.MAX_TICKET_CROWFUNDING_SIMPLE_EUR_ULPS(), "ETO_TERMS_MAX_S_TICKET_EUR_ULPS");
+        }
+        // at least one shre sold
+        require(MIN_NUMBER_OF_TOKENS >= platformTerms.EQUITY_TOKENS_PER_SHARE(), "ETO_TERMS_ONE_SHARE");
+        // duration checks
+        require(DURATION_TERMS.WHITELIST_DURATION() >= platformTerms.MIN_WHITELIST_DURATION_DAYS(), "ETO_TERMS_WL_D_MIN");
+        require(DURATION_TERMS.WHITELIST_DURATION() <= platformTerms.MAX_WHITELIST_DURATION_DAYS(), "ETO_TERMS_WL_D_MAX");
+
+        require(DURATION_TERMS.PUBLIC_DURATION() >= platformTerms.MIN_PUBLIC_DURATION_DAYS(), "ETO_TERMS_PUB_D_MIN");
+        require(DURATION_TERMS.PUBLIC_DURATION() <= platformTerms.MAX_PUBLIC_DURATION_DAYS(), "ETO_TERMS_PUB_D_MAX");
+
+        uint256 totalDuration = DURATION_TERMS.WHITELIST_DURATION() + DURATION_TERMS.PUBLIC_DURATION();
+        require(totalDuration >= platformTerms.MIN_OFFER_DURATION_DAYS(), "ETO_TERMS_TOT_O_MIN");
+        require(totalDuration <= platformTerms.MAX_OFFER_DURATION_DAYS(), "ETO_TERMS_TOT_O_MIN");
+
+        require(DURATION_TERMS.SIGNING_DURATION() >= platformTerms.MIN_SIGNING_DURATION_DAYS(), "ETO_TERMS_SIG_MIN");
+        require(DURATION_TERMS.SIGNING_DURATION() <= platformTerms.MAX_SIGNING_DURATION_DAYS(), "ETO_TERMS_SIG_MAX");
+
+        require(DURATION_TERMS.CLAIM_DURATION() >= platformTerms.MIN_CLAIM_DURATION_DAYS(), "ETO_TERMS_CLAIM_MIN");
+        require(DURATION_TERMS.CLAIM_DURATION() <= platformTerms.MAX_CLAIM_DURATION_DAYS(), "ETO_TERMS_CLAIM_MAX");
+    }
+
+    ////////////////////////
+    // Private methods
+    ////////////////////////
+
+    function addWhitelistInvestorPrivate(
+        address investor,
+        uint256 discountEurUlps,
+        uint256 fixedDiscountFrac
+    )
+        private
+    {
+        // Validate
+        require(investor != address(0));
+        require(fixedDiscountFrac > 0 && fixedDiscountFrac < 2*128);
+        require(discountEurUlps < 2*128);
+
+
+        _whitelist[investor] = WhitelistTicket({
+            discountEurUlps: uint128(discountEurUlps),
+            fixedDiscountFrac: uint128(fixedDiscountFrac)
+        });
+
+        emit LogInvestorWhitelisted(investor, discountEurUlps, fixedDiscountFrac);
     }
 
 }
