7f6664c3d4676544919bd1f7f017a3e6d1f0eca3
Marcin Rudolf <marcin.rudolf@neufund.org>
2018-08-08 13:09:38 +0200

    uses build in abi.encoder

--- contracts/Serialization.sol
+++ contracts/Serialization.sol
@@ -6,22 +6,7 @@ contract Serialization {
     ////////////////////////
     // Internal functions
     ////////////////////////
-    function addressToBytes(address a)
-        internal
-        pure
-        returns (bytes b)
-    {
-        assembly {
-            let m := mload(0x40)
-            // use unsed part of address representation to add length prefix (20) and then position it
-            // so it pretends to be length prefixed 64 bytes "bytes"
-            mstore(add(m, 20), xor(0x140000000000000000000000000000000000000000, a))
-            mstore(0x40, add(m, 52))
-            b := m
-        }
-    }
-
-    function addressFromBytes(bytes b)
+    function decodeAddress(bytes b)
         internal
         pure
         returns (address a)
