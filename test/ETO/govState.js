7f450be2d618d1e800c1f0f342f09cb352e7de96
Marcin Rudolf <marcin.rudolf@neufund.org>
2018-08-09 02:06:23 +0200

    adds govstates for equity token controller

---
+++ test/ETO/govState.js
@@ -0,0 +1,9 @@
+// Needs to match contracts/Company/IControllerGovernance:GovState
+export const GovState = {
+  Setup: 0,
+  Offering: 1,
+  Funded: 2,
+  Closing: 3,
+  Closed: 4,
+  Migrated: 5,
+};
