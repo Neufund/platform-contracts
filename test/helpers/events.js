f53c5335a9594b8271a1124b1f662906dbdc3af0
Marcin Rudolf <marcin.rudolf@neufund.org>
2018-08-09 02:07:08 +0200

    adds additional decoding of events when truffle skips decoding for internal transactions

--- test/helpers/events.js
+++ test/helpers/events.js
@@ -1,4 +1,20 @@
 import { expect } from "chai";
+import SolidityEvent from "web3/lib/web3/event";
+
+// const SolidityEvent = require("");
+
+export function logParser(logs, abi) {
+  // pattern similar to lib/web3/contract.js:  addEventsToContract()
+  const decoders = abi
+    .filter(json => json.type === "event")
+    // note first and third params required only by enocde and execute;
+    // so don't call those!
+    .map(json => new SolidityEvent(null, json, null));
+
+  return logs.map(log =>
+    decoders.find(decoder => decoder.signature() === log.topics[0].replace("0x", "")).decode(log),
+  );
+}
 
 export function hasEvent(tx, eventName) {
   expect(tx).to.have.property("logs");
@@ -15,4 +31,23 @@ export function eventValue(tx, eventName, parName) {
     return event.args[parName];
   }
   return event;
+}
+
+export function eventWithIdxValue(tx, logIdx, eventName, parName) {
+  const events = tx.logs.filter(e => e.event === eventName);
+  expect(events, `Event ${eventName} not found in logs`).to.not.be.empty;
+  expect(events.length, `Event ${eventName} with index ${logIdx} not found in logs`).gte(
+    logIdx + 1,
+  );
+  const event = events[logIdx];
+  if (parName) {
+    expect(event.args, `Parameter ${parName} not in ${eventName} event`).to.have.property(parName);
+    return event.args[parName];
+  }
+  return event;
+}
+
+export function decodeLogs(tx, address, abi) {
+  const logs = tx.receipt.logs.filter(l => l.address === address);
+  return logParser(logs, abi);
 }
