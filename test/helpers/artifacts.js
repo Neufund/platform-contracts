1982c49abadc392be022ee1848ebee0781f6314e
Marcin Rudolf <marcin.rudolf@neufund.org>
2018-08-12 15:10:47 +0200

    artifacts helper only contains artifacts deployed in migration

--- test/helpers/artifacts.js
+++ test/helpers/artifacts.js
@@ -1,4 +1,5 @@
-export default {
+// lists all artifacts that are deployed in migrations (without mocks which are configured in truffle.js)
+export const artifacts = {
   ROLE_BASED_ACCESS_POLICY: "RoleBasedAccessPolicy",
   ETHEREUM_FORK_ARBITER: "EthereumForkArbiter",
   NEUMARK: "Neumark",
@@ -13,9 +14,16 @@ export default {
   EURO_TOKEN_CONTROLLER: "EuroTokenController",
   IDENTITY_REGISTRY: "IdentityRegistry",
   GAS_EXCHANGE: "SimpleExchange",
-  TOKEN_RATE_ORACLE: "ITokenExchangeRateOracle",
-  FEE_DISBURSAL: "IFeeDisbursal",
-  COMMITMENT_INTERFACE: "ETOCommitment",
-  EQUITY_TOKEN: "EquityToken",
-  EQUITY_TOKEN_CONTROLLER: "IEquityTokenController",
+  PLATFORM_TERMS: "PlatformTerms",
+  STANDARD_ETO_COMMITMENT: "ETOCommitment",
+  STANDARD_EQUITY_TOKEN: "EquityToken",
+  PLACEHOLDER_EQUITY_TOKEN_CONTROLLER: "PlaceholderEquityTokenController",
+  STANDARD_ETO_TERMS: "ETOTerms",
+  STANDARD_SHAREHOLDER_RIGHTS: "ShareholderRights",
+  STANDARD_DURATION_TERMS: "ETODurationTerms",
+  // interfaces used in migrations
+  TOKEN_EXCHANGE_RATE_ORACLE: "ITokenExchangeRateOracle",
+  // not implemented
+  // PLATFORM_PORTFOLIO: "IPlatformPortfolio"
+  // FEE_DISBURSAL: "IFeeDisbursal" ,
 };
