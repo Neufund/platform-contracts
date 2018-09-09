712ae6e36da3bfcb0353344882b91ebdcfdea3af
Marcin Rudolf <marcin.rudolf@neufund.org>
2018-09-09 20:32:42 +0200

    adds deployment helpers for ICBM tokens, locked accounts, migrations

--- test/helpers/deployContracts.js
+++ test/helpers/deployContracts.js
@@ -16,6 +16,11 @@ const SimpleExchange = artifacts.require("SimpleExchange");
 const ITokenExchangeRateOracle = artifacts.require("ITokenExchangeRateOracle");
 const IGasExchange = artifacts.require("IGasExchange");
 const PlatformTerms = artifacts.require("PlatformTerms");
+const ICBMLockedAccount = artifacts.require("ICBMLockedAccount");
+const TestICBMLockedAccountController = artifacts.require("TestICBMLockedAccountController");
+const LockedAccount = artifacts.require("LockedAccount");
+const ICBMEtherToken = artifacts.require("ICBMEtherToken");
+const ICBMEuroToken = artifacts.require("ICBMEuroToken");
 
 export async function deployAccessControl(initialRules) {
   const accessPolicy = await RoleBasedAccessPolicy.new();
@@ -107,8 +112,13 @@ export async function deployEuroTokenUniverse(
   maxSimpleExchangeAllowanceEurUlps,
 ) {
   const accessPolicy = await RoleBasedAccessPolicy.at(await universe.accessPolicy());
+  const forkArbiter = await EthereumForkArbiter.at(await universe.forkArbiter());
   const tokenController = await EuroTokenController.new(universe.address);
-  const euroToken = await EuroToken.new(accessPolicy.address, tokenController.address);
+  const euroToken = await EuroToken.new(
+    accessPolicy.address,
+    forkArbiter.address,
+    tokenController.address,
+  );
   await universe.setSingleton(knownInterfaces.euroToken, euroToken.address, {
     from: universeManager,
   });
@@ -124,6 +134,10 @@ export async function deployEuroTokenUniverse(
     maxSimpleExchangeAllowanceEurUlps,
     { from: eurtLegalManager },
   );
+  // amend agreement by legal rep
+  await euroToken.amendAgreement("ipfs:QmPXME1oRtoT627YKaDPDQ3PwA8tdP9rWuAAweLzqSwAWT", {
+    from: eurtLegalManager,
+  });
   return [euroToken, tokenController];
 }
 
@@ -187,4 +201,200 @@ export async function deployPlatformTerms(universe, universeManager, overrideTer
   });
 
   return [platformTerms, terms, termsKeys, termsValues];
+}
+
+export async function applyTransferPermissions(icbmEuroToken, eurtDepositManagr, permissions) {
+  for (const p of permissions) {
+    switch (p.side) {
+      case "from":
+        await icbmEuroToken.setAllowedTransferFrom(p.address, true, {
+          from: eurtDepositManagr,
+        });
+        break;
+      default:
+        await icbmEuroToken.setAllowedTransferTo(p.address, true, {
+          from: eurtDepositManagr,
+        });
+        break;
+    }
+  }
+}
+
+export async function deployICBMLockedAccount(
+  accessPolicy,
+  neumark,
+  lockedAccountManager,
+  paymentToken,
+  feeDisbursalAddress,
+  lockPeriod,
+  unlockPenaltyFraction,
+  { leaveUnlocked = false } = {},
+) {
+  const lockedAccount = await ICBMLockedAccount.new(
+    accessPolicy.address,
+    paymentToken.address,
+    neumark.address,
+    feeDisbursalAddress,
+    lockPeriod,
+    unlockPenaltyFraction,
+  );
+  await accessPolicy.setUserRole(
+    lockedAccountManager,
+    roles.lockedAccountAdmin,
+    lockedAccount.address,
+    TriState.Allow,
+  );
+
+  const controller = await TestICBMLockedAccountController.new(lockedAccount.address);
+  if (!leaveUnlocked) {
+    await lockedAccount.setController(controller.address, {
+      from: lockedAccountManager,
+    });
+  }
+  return [lockedAccount, controller];
+}
+
+export async function deployLockedAccount(
+  universe,
+  icbmLockedAccount,
+  token,
+  universeManager,
+  platformLegalManager,
+) {
+  const lockedAccount = await LockedAccount.new(
+    universe.address,
+    await universe.neumark(),
+    token.address,
+    icbmLockedAccount.address,
+  );
+  // get currency from the token
+  if ((await token.symbol()) === "ETH-T") {
+    await universe.setManySingletons(
+      [knownInterfaces.etherLock, knownInterfaces.icbmEtherLock],
+      [lockedAccount.address, icbmLockedAccount.address],
+      {
+        from: universeManager,
+      },
+    );
+  } else {
+    await universe.setManySingletons(
+      [knownInterfaces.euroLock, knownInterfaces.icbmEuroLock],
+      [lockedAccount.address, icbmLockedAccount.address],
+      {
+        from: universeManager,
+      },
+    );
+  }
+  await lockedAccount.amendAgreement("FAKE AGREEMENT URL", { from: platformLegalManager });
+  return lockedAccount;
+}
+
+export async function deployICBMEuroTokenUniverse(universe, roleManager, universeManager) {
+  const accessPolicy = await RoleBasedAccessPolicy.at(await universe.accessPolicy());
+  const icbmAssetToken = await ICBMEuroToken.new(accessPolicy.address);
+  await accessPolicy.setUserRole(
+    roleManager,
+    roles.eurtDepositManager,
+    icbmAssetToken.address,
+    TriState.Allow,
+  );
+  await universe.setSingleton(knownInterfaces.icbmEuroToken, icbmAssetToken.address, {
+    from: universeManager,
+  });
+  return icbmAssetToken;
+}
+
+export async function deployICBMEtherTokenUniverse(universe, universeManager) {
+  const accessPolicy = await RoleBasedAccessPolicy.at(await universe.accessPolicy());
+  const icbmAssetToken = await ICBMEtherToken.new(accessPolicy.address);
+
+  await universe.setSingleton(knownInterfaces.icbmEtherToken, icbmAssetToken.address, {
+    from: universeManager,
+  });
+  return icbmAssetToken;
+}
+
+export async function deployEuroTokenMigration(
+  universe,
+  admin,
+  operatorWallet,
+  lockPeriod,
+  unlockPenaltyFraction,
+) {
+  const accessPolicy = await RoleBasedAccessPolicy.at(await universe.accessPolicy());
+  const neumark = await Neumark.at(await universe.neumark());
+  const assetToken = await EuroToken.at(await universe.euroToken());
+
+  const icbmAssetToken = await deployICBMEuroTokenUniverse(universe, admin, admin);
+
+  const [icbmLockedAccount, controller] = await deployICBMLockedAccount(
+    accessPolicy,
+    neumark,
+    admin,
+    icbmAssetToken,
+    operatorWallet,
+    lockPeriod,
+    unlockPenaltyFraction,
+  );
+  const lockedAccount = await deployLockedAccount(
+    universe,
+    icbmLockedAccount,
+    assetToken,
+    admin,
+    admin,
+  );
+
+  // euro lock may create deposits during euro token migration
+  await createAccessPolicy(
+    accessPolicy,
+    [
+      {
+        subject: lockedAccount.address,
+        role: roles.eurtDepositManager,
+        object: assetToken.address,
+      },
+    ],
+    { from: admin },
+  );
+  await applyTransferPermissions(icbmAssetToken, admin, [
+    { side: "from", address: icbmLockedAccount.address },
+    { side: "to", address: icbmLockedAccount.address },
+    { side: "from", address: controller.address },
+    { side: "to", address: controller.address },
+    { side: "from", address: lockedAccount.address },
+    { side: "to", address: lockedAccount.address },
+  ]);
+  return [lockedAccount, icbmLockedAccount, icbmAssetToken, controller];
+}
+
+export async function deployEtherTokenMigration(
+  universe,
+  admin,
+  operatorWallet,
+  lockPeriod,
+  unlockPenaltyFraction,
+) {
+  const accessPolicy = await RoleBasedAccessPolicy.at(await universe.accessPolicy());
+  const neumark = await Neumark.at(await universe.neumark());
+  const assetToken = await EuroToken.at(await universe.etherToken());
+
+  const icbmAssetToken = await deployICBMEtherTokenUniverse(universe, admin);
+
+  const [icbmLockedAccount, controller] = await deployICBMLockedAccount(
+    accessPolicy,
+    neumark,
+    admin,
+    icbmAssetToken,
+    operatorWallet,
+    lockPeriod,
+    unlockPenaltyFraction,
+  );
+  const lockedAccount = await deployLockedAccount(
+    universe,
+    icbmLockedAccount,
+    assetToken,
+    admin,
+    admin,
+  );
+  return [lockedAccount, icbmLockedAccount, icbmAssetToken, controller];
 }
