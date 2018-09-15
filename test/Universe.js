4edf1b9955dbfb1b5360a1c6463cf337c026f524
Filip Małachowicz <filip.malachowicz@gmail.com>
2018-09-15 12:37:01 +0200

    Added missing Universe tests with setting universe interface

--- test/Universe.js
+++ test/Universe.js
@@ -562,6 +562,13 @@ contract(
       expect(cid[0]).to.eq(contractId("Universe"));
     });
 
-    it("should revert on setting KNOWN_INTERFACE_UNIVERSE singleton");
+    it("should revert on setting KNOWN_INTERFACE_UNIVERSE singleton", async () => {
+      await expect(universe.setSingleton(knownInterfaces.universe, universe.address)).to.revert;
+    });
+
+    it("should revert on setting KNOWN_INTERFACE_UNIVERSE singleton when setting many singletos", async () => {
+      await expect(universe.setManySingletons([knownInterfaces.universe], [universe.address])).to
+        .revert;
+    });
   },
 );
