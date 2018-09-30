5eba2d50dbf16c9bad924887ecfd025623c606ce
Marcin Rudolf <marcin.rudolf@neufund.org>
2018-09-30 18:28:45 +0200

    implements eto deployment and inspect scripts with commandline support and verification

--- scripts/inspectETO.js
+++ scripts/inspectETO.js
@@ -1,8 +1,27 @@
+/* eslint-disable no-console */
+
 require("babel-register");
+const commandLineArgs = require("command-line-args");
 const checkETO = require("../migrations/deployETO").checkETO;
 const getConfig = require("../migrations/config").getConfig;
 
 module.exports = async function inspectETO() {
-  const CONFIG = getConfig(web3, "localhost", []);
-  await checkETO(artifacts, CONFIG, "0x57f1DC60dfBA309E84e752ff8d08A2B808eaBE4c");
+  const optionDefinitions = [
+    { name: "network", type: String },
+    { name: "eto", type: String },
+    { name: "exec", type: String, multiple: true, defaultOption: true },
+  ];
+
+  let options;
+  try {
+    options = commandLineArgs(optionDefinitions);
+  } catch (e) {
+    console.log(`Invalid command line: ${e}`);
+    console.log(`Expected parameters:`);
+    console.log(optionDefinitions);
+    throw e;
+  }
+
+  const CONFIG = getConfig(web3, options.network, []);
+  await checkETO(artifacts, CONFIG, options.eto);
 };
