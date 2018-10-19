ee8da79cbc8d306f8f36438ce41dee4dbe941d74
Marcin Rudolf <marcin.rudolf@neufund.org>
2018-10-19 13:00:32 +0200

    adds required contracts to flattener

--- scripts/flatten.sh
+++ scripts/flatten.sh
@@ -2,18 +2,20 @@
 # Simple script that flattens all smart contracts up for deployment
 
 #Smart contract paths
-path[0]=../contracts/AccessControl/RoleBasedAccessPolicy.sol
-path[1]=../contracts/EthereumForkArbiter.sol
-path[2]=../contracts/Neumark.sol
-path[3]=../contracts/EtherToken.sol
-path[4]=../contracts/EuroToken.sol
-path[5]=../contracts/LockedAccount.sol
-path[6]=../contracts/Commitment/Commitment.sol
+path[0]=../contracts/PaymentTokens/EtherToken.sol
+path[1]=../contracts/PaymentTokens/EuroToken.sol
+path[2]=../contracts/ICBM/LockedAccount.sol
+path[3]=../contracts/ETO/ETOCommitment.sol
+path[4]=../contracts/Universe.sol
+path[5]=../contracts/Identity/IdentityRegistry.sol
+path[6]=../contracts/SimpleExchange.sol
+path[7]=../contracts/Company/EquityToken.sol
+path[8]=../contracts/Company/PlaceholderEquityTokenController.sol
 
 #Output directory
 output=./build/flatten
 
-for i in {0..6}
+for i in {0..8}
 do
   echo Flattening ${path[i]} to $output
   yarn truffle-flattener ${path[i]} $output
