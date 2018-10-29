064bd4a2b2e7ae3301996c7c62c5e2fac5674d5f
Marcin Rudolf <marcin.rudolf@neufund.org>
2018-10-29 01:02:40 +0100

    adds commitmentObserver getter to IETOCommitmentObserver interface

--- contracts/ETO/IETOCommitmentObserver.sol
+++ contracts/ETO/IETOCommitmentObserver.sol
@@ -6,5 +6,6 @@ import "./IETOCommitmentStates.sol";
 /// @title provides callback on state transitions
 /// @dev observer called after the state() of commitment contract was set
 contract IETOCommitmentObserver is IETOCommitmentStates {
+    function commitmentObserver() public constant returns (address);
     function onStateTransition(ETOState oldState, ETOState newState) public;
 }
