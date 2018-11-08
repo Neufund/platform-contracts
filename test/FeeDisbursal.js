68b37a5e698fb309546ee9de43b7fd4ae91e014d
David <dscharf@gmx.net>
2018-11-08 15:27:28 +0100

    add eurotoken to the happy path test

--- test/FeeDisbursal.js
+++ test/FeeDisbursal.js
@@ -6,6 +6,7 @@ import {
   deployEtherTokenUniverse,
   deployFeeDisbursalUniverse,
   deployNeumarkUniverse,
+  deployEuroTokenUniverse,
 } from "./helpers/deployContracts";
 import { TriState, GLOBAL } from "./helpers/triState";
 import roles from "./helpers/roles";
@@ -31,6 +32,8 @@ contract("FeeDisbursal", ([_, masterManager, disburser, ...investors]) => {
     let identityRegistry;
     let neumark;
     let accessPolicy;
+    let euroToken;
+    let euroTokenController;
 
     /**
      * Setup
@@ -38,15 +41,36 @@ contract("FeeDisbursal", ([_, masterManager, disburser, ...investors]) => {
     beforeEach(async () => {
       identityRegistry = await deployIdentityRegistry(universe, masterManager, masterManager);
       etherToken = await deployEtherTokenUniverse(universe, masterManager);
+      [euroToken, euroTokenController] = await deployEuroTokenUniverse(
+        universe,
+        masterManager,
+        masterManager,
+        masterManager,
+        Q18,
+        Q18,
+        Q18,
+      );
+      neumark = await deployNeumarkUniverse(universe, masterManager);
+
       [feeDisbursal, feeDisbursalController] = await deployFeeDisbursalUniverse(
         universe,
         masterManager,
       );
-      neumark = await deployNeumarkUniverse(universe, masterManager);
 
       // set policy for the disburser
       accessPolicy = await RoleBasedAccessPolicy.at(await universe.accessPolicy());
       await accessPolicy.setUserRole(disburser, roles.disburser, GLOBAL, TriState.Allow);
+
+      // add verified claim for disburser, so he can receive eurotokens
+      await identityRegistry.setClaims(
+        disburser,
+        toBytes32(identityClaims.isNone),
+        toBytes32(identityClaims.isVerified),
+        { from: masterManager },
+      );
+
+      // apply eurotoken controller settings again to update allowed receivers and senders
+      await euroTokenController.applySettings(Q18, Q18, Q18, { from: masterManager });
     });
 
     /**
@@ -66,6 +90,20 @@ contract("FeeDisbursal", ([_, masterManager, disburser, ...investors]) => {
           { from: masterManager },
         );
       }
+    }
+
+    // disburse some ethertokens from the given disburser
+    async function disburseEtherToken(sender, amount) {
+      return etherToken.depositAndTransfer(feeDisbursal.address, amount, 0, {
+        from: sender,
+        value: amount,
+      });
+    }
+
+    async function disburseEuroToken(sender, amount) {
+      return euroToken.depositAndTransfer(sender, feeDisbursal.address, amount, amount, 0, 0, {
+        from: masterManager,
+      });
     }
 
     /**
@@ -118,15 +156,16 @@ contract("FeeDisbursal", ([_, masterManager, disburser, ...investors]) => {
       await prepareInvestor(investors[1], Q18.mul(300), true);
       await prepareInvestor(investors[2], Q18.mul(500), true);
       await assertDisbursalCount(etherToken, 0);
+      await assertDisbursalCount(euroToken, 0);
 
       // disburse some ether tokens from the disburser
-      await etherToken.depositAndTransfer(feeDisbursal.address, Q18.mul(100), 0, {
-        from: disburser,
-        value: Q18.mul(100),
-      });
+      await disburseEtherToken(disburser, Q18.mul(100));
+      await disburseEuroToken(disburser, Q18.mul(50));
       // there now should be the full amount of ethertokens as well as the count of disbursals for this token here
       await assertTokenBalance(etherToken, feeDisbursal.address, Q18.mul(100));
       await assertDisbursalCount(etherToken, 1);
+      await assertTokenBalance(euroToken, feeDisbursal.address, Q18.mul(50));
+      await assertDisbursalCount(euroToken, 1);
 
       increaseTime(60 * 60 * 24);
 
@@ -134,18 +173,17 @@ contract("FeeDisbursal", ([_, masterManager, disburser, ...investors]) => {
       await assertClaimable(etherToken, investors[0], 1000, Q18.mul(20));
       await assertClaimable(etherToken, investors[1], 1000, Q18.mul(30));
       await assertClaimable(etherToken, investors[2], 1000, Q18.mul(50));
+      await assertClaimable(euroToken, investors[0], 1000, Q18.mul(10));
+      await assertClaimable(euroToken, investors[1], 1000, Q18.mul(15));
+      await assertClaimable(euroToken, investors[2], 1000, Q18.mul(25));
 
       // disburse some more ether
-      await etherToken.depositAndTransfer(feeDisbursal.address, Q18.mul(150), 0, {
-        from: disburser,
-        value: Q18.mul(150),
-      });
-      await etherToken.depositAndTransfer(feeDisbursal.address, Q18.mul(250), 0, {
-        from: disburser,
-        value: Q18.mul(250),
-      });
+      await disburseEtherToken(disburser, Q18.mul(150));
+      await disburseEtherToken(disburser, Q18.mul(250));
+      await disburseEuroToken(disburser, Q18.mul(200));
       // the last two disbursals should have been merged, so we now have 2 disbursals in total
       await assertDisbursalCount(etherToken, 2);
+      await assertDisbursalCount(euroToken, 2);
 
       increaseTime(60 * 60 * 24);
 
@@ -153,26 +191,42 @@ contract("FeeDisbursal", ([_, masterManager, disburser, ...investors]) => {
       await assertClaimable(etherToken, investors[0], 1000, Q18.mul(100));
       await assertClaimable(etherToken, investors[1], 1000, Q18.mul(150));
       await assertClaimable(etherToken, investors[2], 1000, Q18.mul(250));
+      await assertClaimable(euroToken, investors[0], 1000, Q18.mul(50));
+      await assertClaimable(euroToken, investors[1], 1000, Q18.mul(75));
+      await assertClaimable(euroToken, investors[2], 1000, Q18.mul(125));
 
       // claim first and check claimable balances
       await feeDisbursal.claim(etherToken.address, Q18, { from: investors[0] });
+      await feeDisbursal.claim(euroToken.address, Q18, { from: investors[1] });
       await assertClaimable(etherToken, investors[0], 1000, 0);
       await assertClaimable(etherToken, investors[1], 1000, Q18.mul(150));
       await assertClaimable(etherToken, investors[2], 1000, Q18.mul(250));
+      await assertClaimable(euroToken, investors[0], 1000, Q18.mul(50));
+      await assertClaimable(euroToken, investors[1], 1000, Q18.mul(0));
+      await assertClaimable(euroToken, investors[2], 1000, Q18.mul(125));
 
       // claim the rest
       await feeDisbursal.claim(etherToken.address, Q18, { from: investors[1] });
       await feeDisbursal.claim(etherToken.address, Q18, { from: investors[2] });
+      await feeDisbursal.claim(euroToken.address, Q18, { from: investors[0] });
+      await feeDisbursal.claim(euroToken.address, Q18, { from: investors[2] });
+
       await assertClaimable(etherToken, investors[1], 1000, 0);
       await assertClaimable(etherToken, investors[2], 1000, 0);
+      await assertClaimable(euroToken, investors[1], 1000, 0);
+      await assertClaimable(euroToken, investors[2], 1000, 0);
 
       // assert token balances after payout
       await assertTokenBalance(etherToken, investors[0], Q18.mul(100));
       await assertTokenBalance(etherToken, investors[1], Q18.mul(150));
       await assertTokenBalance(etherToken, investors[2], Q18.mul(250));
+      await assertTokenBalance(euroToken, investors[0], Q18.mul(50));
+      await assertTokenBalance(euroToken, investors[1], Q18.mul(75));
+      await assertTokenBalance(euroToken, investors[2], Q18.mul(125));
 
       // all ether is payed out now
       await assertTokenBalance(etherToken, feeDisbursal.address, Q18.mul(0));
+      await assertTokenBalance(euroToken, feeDisbursal.address, Q18.mul(0));
     });
 
     it("should support simple claims merging and step by step claim payout", async () => {
@@ -182,18 +236,12 @@ contract("FeeDisbursal", ([_, masterManager, disburser, ...investors]) => {
       await assertClaimable(etherToken, investors[0], 1000, Q18.mul(0));
 
       // first disbursal
-      await etherToken.depositAndTransfer(feeDisbursal.address, Q18.mul(15), 0, {
-        from: disburser,
-        value: Q18.mul(150),
-      });
+      await disburseEtherToken(disburser, Q18.mul(15));
       // we now have one disbursal, but nothing claimable, as the claim is not sealed yet
       await assertDisbursalCount(etherToken, 1);
       await assertClaimable(etherToken, investors[0], 1000, Q18.mul(0));
       // second disbursal, should be merged with first
-      await etherToken.depositAndTransfer(feeDisbursal.address, Q18.mul(5), 0, {
-        from: disburser,
-        value: Q18.mul(150),
-      });
+      await disburseEtherToken(disburser, Q18.mul(5));
       await assertDisbursalCount(etherToken, 1);
       await assertClaimable(etherToken, investors[0], 1000, Q18.mul(0));
       // after one day these funds become claimable
@@ -201,14 +249,8 @@ contract("FeeDisbursal", ([_, masterManager, disburser, ...investors]) => {
       await assertDisbursalCount(etherToken, 1);
       await assertClaimable(etherToken, investors[0], 1000, Q18.mul(20));
       // some more disbursing
-      await etherToken.depositAndTransfer(feeDisbursal.address, Q18.mul(5), 0, {
-        from: disburser,
-        value: Q18.mul(150),
-      });
-      await etherToken.depositAndTransfer(feeDisbursal.address, Q18.mul(5), 0, {
-        from: disburser,
-        value: Q18.mul(150),
-      });
+      await disburseEtherToken(disburser, Q18.mul(5));
+      await disburseEtherToken(disburser, Q18.mul(5));
       // both should now be merged, but not claimable yet
       await assertDisbursalCount(etherToken, 2);
       await assertClaimable(etherToken, investors[0], 1000, Q18.mul(20));
@@ -216,10 +258,7 @@ contract("FeeDisbursal", ([_, masterManager, disburser, ...investors]) => {
       // claimable now
       await assertClaimable(etherToken, investors[0], 1000, Q18.mul(30));
       // add another day with disbursal
-      await etherToken.depositAndTransfer(feeDisbursal.address, Q18.mul(20), 0, {
-        from: disburser,
-        value: Q18.mul(150),
-      });
+      await disburseEtherToken(disburser, Q18.mul(20));
       increaseTime(60 * 60 * 24);
       await assertDisbursalCount(etherToken, 3);
       await assertClaimable(etherToken, investors[0], 1000, Q18.mul(50));
@@ -247,12 +286,8 @@ contract("FeeDisbursal", ([_, masterManager, disburser, ...investors]) => {
       await prepareInvestor(investors[0], Q18.mul(200), true);
       const testDisburser = investors[1];
       // without role or being eto commitment, this won't work
-      await expect(
-        etherToken.depositAndTransfer(feeDisbursal.address, Q18.mul(1), 0, {
-          from: testDisburser,
-          value: Q18.mul(100),
-        }),
-      ).to.revert;
+      await expect(disburseEtherToken(testDisburser, Q18.mul(1))).to.revert;
+
       // eto commitment interface will work
       await universe.setCollectionInterface(
         knownInterfaces.commitmentInterface,
