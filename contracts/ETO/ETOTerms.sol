751e12fa39c7cdf3f9062b834b3e77b81b2431d4
Marcin Rudolf <marcin.rudolf@neufund.org>
2018-11-18 19:24:22 +0100

    refactors ETOTerms to add support for non-retail ETO
    (1) adds KYC check
    (2) disabled token transferability for retail investor
    (3) enforces large MIN_QUALIFIED_INVESTOR_TICKET_EUR_ULPS min ticket for non-retail
    (4) adds public discount
    (5) renames PROSPECTUS_URL
    (6) removes INVESTMENT_AGREEMENT_TEMPLATE_URL

--- contracts/ETO/ETOTerms.sol
+++ contracts/ETO/ETOTerms.sol
@@ -6,12 +6,17 @@ import "../Standards/IContractId.sol";
 import "../PlatformTerms.sol";
 import "../Company/ShareholderRights.sol";
 import "../Math.sol";
+import "../Universe.sol";
 
 
 /// @title base terms of Equity Token Offering
 /// encapsulates pricing, discounts and whitelisting mechanism
 /// @dev to be split is mixins
-contract ETOTerms is Math, IContractId {
+contract ETOTerms is
+    IdentityRecord,
+    Math,
+    IContractId
+{
 
     ////////////////////////
     // Types
@@ -30,6 +35,7 @@ contract ETOTerms is Math, IContractId {
     ////////////////////////
 
     bytes32 private constant EMPTY_STRING_HASH = 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470;
+    uint256 public constant MIN_QUALIFIED_INVESTOR_TICKET_EUR_ULPS = 100000 * 10**18;
 
     ////////////////////////
     // Immutable state
@@ -56,20 +62,30 @@ contract ETOTerms is Math, IContractId {
     // if set to False transfers on Equity Token will remain disabled after offering
     // once those terms are on-chain this flags fully controls token transferability
     bool public ENABLE_TRANSFERS_ON_SUCCESS;
+    // tells if offering accepts retail investors. if so, registered prospectus is required
+    // and ENABLE_TRANSFERS_ON_SUCCESS is forced to be false as per current platform policy
+    bool public ALLOW_RETAIL_INVESTORS;
     // represents the discount % for whitelist participants
     uint256 public WHITELIST_DISCOUNT_FRAC;
+    // represents the discount % for public participants, using values > 0 will result
+    // in automatic downround shareholder resolution
+    uint256 public PUBLIC_DISCOUNT_FRAC;
 
     // paperwork
-    // url (typically IPFS hash) to investment agreement between nominee and company
-    string public INVESTMENT_AGREEMENT_TEMPLATE_URL;
-    // prospectus url
-    string public PROSPECTUS_URL;
+    // prospectus / investment memorandum / crowdfunding pamphlet etc.
+    string public INVESTOR_OFFERING_DOCUMENT_URL;
     // settings for shareholder rights
     ShareholderRights public SHAREHOLDER_RIGHTS;
 
     // equity token setup
     string public EQUITY_TOKEN_NAME;
     string public EQUITY_TOKEN_SYMBOL;
+
+    // manages whitelist
+    address public WHITELIST_MANAGER;
+    // wallet registry of KYC procedure
+    IIdentityRegistry public IDENTITY_REGISTRY;
+    Universe public UNIVERSE;
 
     // variables from token terms for local use
     // minimum number of tokens being offered. will set min cap
@@ -79,8 +95,6 @@ contract ETOTerms is Math, IContractId {
     // base token price in EUR-T, without any discount scheme
     uint256 private TOKEN_PRICE_EUR_ULPS;
 
-    // manages whitelist
-    address private WHITELIST_MANAGER;
 
     ////////////////////////
     // Mutable state
@@ -114,39 +128,37 @@ contract ETOTerms is Math, IContractId {
     ////////////////////////
 
     constructor(
+        Universe universe,
         ETODurationTerms durationTerms,
         ETOTokenTerms tokenTerms,
         uint256 existingCompanyShares,
         uint256 minTicketEurUlps,
         uint256 maxTicketEurUlps,
+        bool allowRetailInvestors,
         bool enableTransfersOnSuccess,
-        string investmentAgreementTemplateUrl,
-        string prospectusUrl,
+        string investorOfferingDocumentUrl,
         ShareholderRights shareholderRights,
         string equityTokenName,
         string equityTokenSymbol,
         uint256 shareNominalValueEurUlps,
-        uint256 whitelistDiscountFrac
+        uint256 whitelistDiscountFrac,
+        uint256 publicDiscountFrac
     )
         public
     {
         require(durationTerms != address(0));
         require(tokenTerms != address(0));
         require(existingCompanyShares > 0);
-        require(keccak256(abi.encodePacked(prospectusUrl)) != EMPTY_STRING_HASH);
-        require(keccak256(abi.encodePacked(investmentAgreementTemplateUrl)) != EMPTY_STRING_HASH);
+        require(keccak256(abi.encodePacked(investorOfferingDocumentUrl)) != EMPTY_STRING_HASH);
         require(keccak256(abi.encodePacked(equityTokenName)) != EMPTY_STRING_HASH);
         require(keccak256(abi.encodePacked(equityTokenSymbol)) != EMPTY_STRING_HASH);
         require(shareholderRights != address(0));
         // test interface
-        require(shareholderRights.HAS_GENERAL_INFORMATION_RIGHTS());
+        // require(shareholderRights.HAS_GENERAL_INFORMATION_RIGHTS());
         require(shareNominalValueEurUlps > 0);
         require(whitelistDiscountFrac >= 0 && whitelistDiscountFrac <= 99*10**16);
+        require(publicDiscountFrac >= 0 && publicDiscountFrac <= 99*10**16);
         require(minTicketEurUlps<=maxTicketEurUlps);
-        // min ticket must be > token price
-        require(minTicketEurUlps >= tokenTerms.TOKEN_PRICE_EUR_ULPS(), "NF_MIN_TICKET_LT_TOKEN_PRICE");
-        // it must be possible to collect more funds than max number of tokens
-        require(tokenTerms.MAX_NUMBER_OF_TOKENS() * tokenTerms.TOKEN_PRICE_EUR_ULPS() >= minTicketEurUlps, "NF_MAX_FUNDS_LT_MIN_TICKET");
 
         // copy token terms variables
         MIN_NUMBER_OF_TOKENS = tokenTerms.MIN_NUMBER_OF_TOKENS();
@@ -158,15 +170,18 @@ contract ETOTerms is Math, IContractId {
         EXISTING_COMPANY_SHARES = existingCompanyShares;
         MIN_TICKET_EUR_ULPS = minTicketEurUlps;
         MAX_TICKET_EUR_ULPS = maxTicketEurUlps;
+        ALLOW_RETAIL_INVESTORS = allowRetailInvestors;
         ENABLE_TRANSFERS_ON_SUCCESS = enableTransfersOnSuccess;
-        INVESTMENT_AGREEMENT_TEMPLATE_URL = investmentAgreementTemplateUrl;
-        PROSPECTUS_URL = prospectusUrl;
+        INVESTOR_OFFERING_DOCUMENT_URL = investorOfferingDocumentUrl;
         SHAREHOLDER_RIGHTS = shareholderRights;
         EQUITY_TOKEN_NAME = equityTokenName;
         EQUITY_TOKEN_SYMBOL = equityTokenSymbol;
         SHARE_NOMINAL_VALUE_EUR_ULPS = shareNominalValueEurUlps;
         WHITELIST_DISCOUNT_FRAC = whitelistDiscountFrac;
+        PUBLIC_DISCOUNT_FRAC = publicDiscountFrac;
         WHITELIST_MANAGER = msg.sender;
+        IDENTITY_REGISTRY = IIdentityRegistry(universe.identityRegistry());
+        UNIVERSE = universe;
     }
 
     ////////////////////////
@@ -181,7 +196,7 @@ contract ETOTerms is Math, IContractId {
         returns (uint256 tokenAmountInt)
     {
         // we may disregard totalEurUlps as curve is flat, round down when calculating tokens
-        return committedEurUlps / TOKEN_PRICE_EUR_ULPS;
+        return committedEurUlps / calculatePriceFraction(10**18 - PUBLIC_DISCOUNT_FRAC);
     }
 
     // calculates amount of euro required to acquire amount of tokens at a position of the (inverse) curve
@@ -192,7 +207,7 @@ contract ETOTerms is Math, IContractId {
         returns (uint256 committedEurUlps)
     {
         // we may disregard totalTokensInt as curve is flat
-        return mul(tokenAmountInt, TOKEN_PRICE_EUR_ULPS);
+        return mul(tokenAmountInt, calculatePriceFraction(10**18 - PUBLIC_DISCOUNT_FRAC));
     }
 
     // get mincap in EUR
@@ -203,6 +218,14 @@ contract ETOTerms is Math, IContractId {
     // get max cap in EUR
     function ESTIMATED_MAX_CAP_EUR_ULPS() public constant returns(uint256) {
         return calculateEurUlpsAmount(0, MAX_NUMBER_OF_TOKENS);
+    }
+
+    function calculatePriceFraction(uint256 priceFrac) public constant returns(uint256) {
+        if (priceFrac == 1) {
+            return TOKEN_PRICE_EUR_ULPS;
+        } else {
+            return decimalFraction(priceFrac, TOKEN_PRICE_EUR_ULPS);
+        }
     }
 
     function addWhitelisted(
@@ -244,43 +267,28 @@ contract ETOTerms is Math, IContractId {
         constant
         returns (
             bool isWhitelisted,
+            bool isEligible,
             uint256 minTicketEurUlps,
             uint256 maxTicketEurUlps,
             uint256 equityTokenInt,
             uint256 fixedSlotEquityTokenInt
             )
     {
-        uint256 discountedAmount;
-        minTicketEurUlps = MIN_TICKET_EUR_ULPS;
-        maxTicketEurUlps = MAX_TICKET_EUR_ULPS;
-        // whitelist use discount is possible
-        if (applyWhitelistDiscounts) {
-            WhitelistTicket storage wlTicket = _whitelist[investor];
-            // check if has access to discount
-            isWhitelisted = wlTicket.fullTokenPriceFrac > 0;
-            maxTicketEurUlps = max(wlTicket.discountAmountEurUlps, MAX_TICKET_EUR_ULPS);
-            if (existingInvestorContributionEurUlps < wlTicket.discountAmountEurUlps) {
-                discountedAmount = min(newInvestorContributionEurUlps, wlTicket.discountAmountEurUlps - existingInvestorContributionEurUlps);
-                // discount is fixed so use base token price
-                if (discountedAmount > 0) {
-                    // always round down when calculating tokens
-                    fixedSlotEquityTokenInt = discountedAmount / decimalFraction(wlTicket.fullTokenPriceFrac, TOKEN_PRICE_EUR_ULPS);
-                }
-            }
-        }
-        // if any amount above discount
-        uint256 remainingAmount = newInvestorContributionEurUlps - discountedAmount;
-        if (remainingAmount > 0) {
-            if (applyWhitelistDiscounts && WHITELIST_DISCOUNT_FRAC > 0) {
-                // will not overflow, WHITELIST_DISCOUNT_FRAC < Q18 from constructor, also round down
-                equityTokenInt = remainingAmount / decimalFraction(10**18 - WHITELIST_DISCOUNT_FRAC, TOKEN_PRICE_EUR_ULPS);
-            } else {
-                // use pricing along the curve
-                equityTokenInt = calculateTokenAmount(totalContributedEurUlps + discountedAmount, remainingAmount);
-            }
-        }
-        // should have all issued tokens
-        equityTokenInt += fixedSlotEquityTokenInt;
+        (
+            isWhitelisted,
+            minTicketEurUlps,
+            maxTicketEurUlps,
+            equityTokenInt,
+            fixedSlotEquityTokenInt
+        ) = calculateContributionPrivate(
+            investor,
+            totalContributedEurUlps,
+            existingInvestorContributionEurUlps,
+            newInvestorContributionEurUlps,
+            applyWhitelistDiscounts);
+        // check if is eligible for investment
+        IdentityClaims memory claims = deserializeClaims(IDENTITY_REGISTRY.getClaims(investor));
+        isEligible = claims.isVerified && !claims.accountFrozen;
     }
 
     function equityTokensToShares(uint256 amount)
@@ -295,10 +303,22 @@ contract ETOTerms is Math, IContractId {
     function requireValidTerms(PlatformTerms platformTerms)
         public
         constant
+        returns (bool)
     {
+        // apply constraints on retail fundraising
+        if (ALLOW_RETAIL_INVESTORS) {
+            // make sure transfers are disabled after offering for retail investors
+            require(!ENABLE_TRANSFERS_ON_SUCCESS, "NF_MUST_DISABLE_TRANSFERS");
+        } else {
+            // only qualified investors allowed defined as tickets > 100000 EUR
+            require(MIN_TICKET_EUR_ULPS >= MIN_QUALIFIED_INVESTOR_TICKET_EUR_ULPS, "NF_MIN_QUALIFIED_INVESTOR_TICKET");
+        }
+        // min ticket must be > token price
+        require(MIN_TICKET_EUR_ULPS >= TOKEN_TERMS.TOKEN_PRICE_EUR_ULPS(), "NF_MIN_TICKET_LT_TOKEN_PRICE");
+        // it must be possible to collect more funds than max number of tokens
+        require(ESTIMATED_MAX_CAP_EUR_ULPS() >= MIN_TICKET_EUR_ULPS, "NF_MAX_FUNDS_LT_MIN_TICKET");
+
         require(MIN_TICKET_EUR_ULPS >= platformTerms.MIN_TICKET_EUR_ULPS(), "NF_ETO_TERMS_MIN_TICKET_EUR_ULPS");
-        // at least one share sold
-        require(MIN_NUMBER_OF_TOKENS >= TOKEN_TERMS.EQUITY_TOKENS_PER_SHARE(), "NF_ETO_TERMS_ONE_SHARE");
         // duration checks
         require(DURATION_TERMS.WHITELIST_DURATION() >= platformTerms.MIN_WHITELIST_DURATION(), "NF_ETO_TERMS_WL_D_MIN");
         require(DURATION_TERMS.WHITELIST_DURATION() <= platformTerms.MAX_WHITELIST_DURATION(), "NF_ETO_TERMS_WL_D_MAX");
@@ -315,6 +335,8 @@ contract ETOTerms is Math, IContractId {
 
         require(DURATION_TERMS.CLAIM_DURATION() >= platformTerms.MIN_CLAIM_DURATION(), "NF_ETO_TERMS_CLAIM_MIN");
         require(DURATION_TERMS.CLAIM_DURATION() <= platformTerms.MAX_CLAIM_DURATION(), "NF_ETO_TERMS_CLAIM_MAX");
+
+        return true;
     }
 
     //
@@ -328,6 +350,62 @@ contract ETOTerms is Math, IContractId {
     ////////////////////////
     // Private methods
     ////////////////////////
+
+    function calculateContributionPrivate(
+        address investor,
+        uint256 totalContributedEurUlps,
+        uint256 existingInvestorContributionEurUlps,
+        uint256 newInvestorContributionEurUlps,
+        bool applyWhitelistDiscounts
+    )
+        private
+        constant
+        returns (
+            bool isWhitelisted,
+            uint256 minTicketEurUlps,
+            uint256 maxTicketEurUlps,
+            uint256 equityTokenInt,
+            uint256 fixedSlotEquityTokenInt
+        )
+    {
+        uint256 discountedAmount;
+        minTicketEurUlps = MIN_TICKET_EUR_ULPS;
+        maxTicketEurUlps = MAX_TICKET_EUR_ULPS;
+        // whitelist use discount is possible
+        if (applyWhitelistDiscounts) {
+            WhitelistTicket storage wlTicket = _whitelist[investor];
+            // check if has access to discount
+            isWhitelisted = wlTicket.fullTokenPriceFrac > 0;
+            // can invest more than general max ticket
+            maxTicketEurUlps = max(wlTicket.discountAmountEurUlps, maxTicketEurUlps);
+            // can invest less than general min ticket
+            if (wlTicket.discountAmountEurUlps > 0) {
+                minTicketEurUlps = min(wlTicket.discountAmountEurUlps, minTicketEurUlps);
+            }
+            if (existingInvestorContributionEurUlps < wlTicket.discountAmountEurUlps) {
+                discountedAmount = min(newInvestorContributionEurUlps, wlTicket.discountAmountEurUlps - existingInvestorContributionEurUlps);
+                // discount is fixed so use base token price
+                if (discountedAmount > 0) {
+                    // always round down when calculating tokens
+                    fixedSlotEquityTokenInt = discountedAmount / calculatePriceFraction(wlTicket.fullTokenPriceFrac);
+                }
+            }
+        }
+        // if any amount above discount
+        uint256 remainingAmount = newInvestorContributionEurUlps - discountedAmount;
+        if (remainingAmount > 0) {
+            if (applyWhitelistDiscounts && WHITELIST_DISCOUNT_FRAC > 0) {
+                // will not overflow, WHITELIST_DISCOUNT_FRAC < Q18 from constructor, also round down
+                equityTokenInt = remainingAmount / calculatePriceFraction(10**18 - WHITELIST_DISCOUNT_FRAC);
+            } else {
+                // use pricing along the curve
+                equityTokenInt = calculateTokenAmount(totalContributedEurUlps + discountedAmount, remainingAmount);
+            }
+        }
+        // should have all issued tokens
+        equityTokenInt += fixedSlotEquityTokenInt;
+
+    }
 
     function addWhitelistInvestorPrivate(
         address investor,
