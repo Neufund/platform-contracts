db4cab9b8de5b6429e62ee4d63b186dd317b8a5e
Filip Małachowicz <filip.malachowicz@gmail.com>
2019-05-19 19:21:18 +0200

    Reschedule migrations

---
+++ migrations/7_distrubute_ether_to_fixtures_accounts.js
@@ -0,0 +1,29 @@
+require("babel-register");
+const getConfig = require("./config").getConfig;
+const getFixtureAccounts = require("./config").getFixtureAccounts;
+const getDeployerAccount = require("./config").getDeployerAccount;
+const promisify = require("../test/helpers/evmCommands").promisify;
+
+module.exports = function deployContracts(deployer, network, accounts) {
+  const CONFIG = getConfig(web3, network, accounts);
+  if (CONFIG.shouldSkipStep(__filename)) return;
+  if (CONFIG.isLiveDeployment) return;
+
+  const DEPLOYER = getDeployerAccount(network, accounts);
+  deployer.then(async () => {
+    const fas = getFixtureAccounts();
+
+    console.log("Distribute ether to fixtures accounts");
+    for (const f of Object.keys(fas)) {
+      const valueToSend = web3.toWei(10, "ether");
+
+      await promisify(web3.eth.sendTransaction)({
+        from: DEPLOYER,
+        to: fas[f].address,
+        value: valueToSend,
+        gasPrice: 100,
+        gas: 21000,
+      });
+    }
+  });
+};
