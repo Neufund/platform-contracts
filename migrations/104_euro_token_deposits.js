0467f7468b01649e28a9230bbf82272e0fc553d5
Filip Małachowicz <filip.malachowicz@gmail.com>
2019-05-21 21:17:42 +0200

    ETH-T, EUR-T deposits and ICMB locked accounts migrations based on fixtures_accounts_definitions.json

---
+++ migrations/104_euro_token_deposits.js
@@ -0,0 +1,36 @@
+require("babel-register");
+const getConfig = require("./config").getConfig;
+const getFixtureAccounts = require("./getFixtureAccounts").getFixtureAccounts;
+const getDeployerAccount = require("./config").getDeployerAccount;
+
+module.exports = function deployContracts(deployer, network, accounts) {
+  const CONFIG = getConfig(web3, network, accounts);
+  if (CONFIG.shouldSkipStep(__filename)) return;
+
+  const DEPLOYER = getDeployerAccount(network, accounts);
+
+  const EuroToken = artifacts.require(CONFIG.artifacts.EURO_TOKEN);
+  const Universe = artifacts.require(CONFIG.artifacts.UNIVERSE);
+
+  deployer.then(async () => {
+    // skip for pure live deployment
+    if (CONFIG.isLiveDeployment && !CONFIG.ISOLATED_UNIVERSE) return;
+
+    const universe = await Universe.deployed();
+    const euroToken = await EuroToken.at(await universe.euroToken());
+
+    const fas = getFixtureAccounts(accounts);
+
+    console.log("Deposit in EuroToken from fixture accounts");
+    for (const f of Object.keys(fas)) {
+      if (fas[f].balances && fas[f].balances.euroToken && fas[f].balances.euroToken > 0) {
+        await euroToken.deposit(fas[f].address, CONFIG.Q18.mul(fas[f].balances.euroToken), "0x0", {
+          from: DEPLOYER,
+        });
+
+        const balance = await euroToken.balanceOf(fas[f].address);
+        console.log(`Account ${f} has ${balance.div(CONFIG.Q18).toString()} EUR-T.`);
+      }
+    }
+  });
+};
---
+++ migrations/105_migrate_locked_icbm_accounts.js
@@ -0,0 +1,50 @@
+require("babel-register");
+const getConfig = require("./config").getConfig;
+const getFixtureAccounts = require("./getFixtureAccounts").getFixtureAccounts;
+
+module.exports = function deployContracts(deployer, network, accounts) {
+  const CONFIG = getConfig(web3, network, accounts);
+  if (CONFIG.shouldSkipStep(__filename)) return;
+
+  const ICBMEuroToken = artifacts.require(CONFIG.artifacts.ICBM_EURO_TOKEN);
+  const Universe = artifacts.require(CONFIG.artifacts.UNIVERSE);
+  const ICBMLockedAccount = artifacts.require(CONFIG.artifacts.ICBM_LOCKED_ACCOUNT);
+  const LockedAccount = artifacts.require(CONFIG.artifacts.LOCKED_ACCOUNT);
+
+  deployer.then(async () => {
+    // skip for pure live deployment
+    if (CONFIG.isLiveDeployment && !CONFIG.ISOLATED_UNIVERSE) return;
+
+    const universe = await Universe.deployed();
+    const euroLock = await LockedAccount.at(await universe.euroLock());
+    const icbmEuroLock = await ICBMLockedAccount.at(await universe.icbmEuroLock());
+    const icbmEtherLock = await ICBMLockedAccount.at(await universe.icbmEtherLock());
+    const icbmEuroToken = await ICBMEuroToken.at(await icbmEuroLock.assetToken());
+
+    const fas = getFixtureAccounts(accounts);
+
+    console.log("let euroLock to receive and send old euro token");
+    await icbmEuroToken.setAllowedTransferFrom(euroLock.address, true);
+    await icbmEuroToken.setAllowedTransferTo(euroLock.address, true);
+
+    console.log("migrating locked accounts");
+    for (const f of Object.keys(fas)) {
+      if (fas[f].icbmMigrations && fas[f].icbmMigrations.etherToken) {
+        await icbmEtherLock.migrate({ from: fas[f].address });
+        const lockedEthbalanceResult = await icbmEtherLock.balanceOf(fas[f].address);
+        console.log(
+          `Account ${f} has ${lockedEthbalanceResult[0].div(CONFIG.Q18).toString()} ETH-T locked.`,
+        );
+      }
+
+      if (fas[f].icbmMigrations && fas[f].icbmMigrations.euroToken) {
+        await icbmEuroLock.migrate({ from: fas[f].address });
+        const lockedEuroBalanceResult = await icbmEtherLock.balanceOf(fas[f].address);
+        console.log(`${lockedEuroBalanceResult}`);
+        console.log(
+          `Account ${f} has ${lockedEuroBalanceResult[0].div(CONFIG.Q18).toString()} EUR-T locked.`,
+        );
+      }
+    }
+  });
+};
