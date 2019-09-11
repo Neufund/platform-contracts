10c7014cc069d9fbd7016439f3dcab6bcdc9d495
Marcin Rudolf <marcin.rudolf@neufund.org>
2019-09-11 21:49:38 +0200

    removes shareNominalValueEurUlps and introduces shareNominalValueUlps that represent nominal value of shares in ISHA currency, makes constructor much nicer

--- contracts/Company/IEquityToken.sol
+++ contracts/Company/IEquityToken.sol
@@ -21,8 +21,8 @@ contract IEquityToken is
     // number of shares represented by tokens. we round to the closest value.
     function sharesTotalSupply() public constant returns (uint256);
 
-    /// nominal value of a share in EUR decimal(18) precision
-    function shareNominalValueEurUlps() public constant returns (uint256);
+    /// nominal value of a share in decimal(18) precision in currency as per token controller ISHA
+    function shareNominalValueUlps() public constant returns (uint256);
 
     // returns company legal representative account that never changes
     function companyLegalRepresentative() public constant returns (address);
