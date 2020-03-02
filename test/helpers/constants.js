9cdb8dbd86b380a2e4f63e50d311cf5c3f8f31b2
Marcin Rudolf <marcin.rudolf@neufund.org>
2020-03-02 16:56:20 +0100

    defines default token decimals in constants

--- test/helpers/constants.js
+++ test/helpers/constants.js
@@ -2,12 +2,21 @@ const Web3 = require("web3");
 
 export const web3 = new Web3();
 export const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
-export const Q18 = web3.toBigNumber("10").pow(18);
+export const decimalBase = new web3.BigNumber("10");
+export const Q18 = decimalBase.pow(18);
 export const hourInSeconds = 60 * 60;
 export const dayInSeconds = 24 * hourInSeconds;
 export const monthInSeconds = 30 * dayInSeconds;
 export const daysToSeconds = sec => sec * dayInSeconds;
 export const hoursToSeconds = sec => sec * hourInSeconds;
+// default precision and power of equity token as used by fixtures and default test terms
+// allow to override from env variable to run ETO tests several precisions
+export const defEquityTokenPrecision = new web3.BigNumber(
+  process.env.EQUITY_TOKEN_PRECISION || "18",
+);
+export const defEquityTokenPower = decimalBase.pow(defEquityTokenPrecision);
+// default tokens per share used as above
+export const defaultTokensPerShare = defEquityTokenPower.mul(new web3.BigNumber("1000000"));
 
 export function toBytes32(hexOrNumber) {
   let strippedHex = "0";
