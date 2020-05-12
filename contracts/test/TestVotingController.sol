47cce804b7b7e3ed73b744edc58dccf0732746a2
Marcin Rudolf <marcin.rudolf@neufund.org>
2020-05-12 23:47:53 +0200

    implements test voting controller and observer

---
+++ contracts/test/TestVotingController.sol
@@ -0,0 +1,44 @@
+pragma solidity 0.4.26;
+
+import "../Company/Extras/IVotingController.sol";
+import "../Universe.sol";
+import "../KnownContracts.sol";
+
+
+contract TestVotingController is IVotingController, KnownContracts {
+
+    ////////////////////////
+    // Constructor
+    ////////////////////////
+    constructor(Universe /*universe*/)
+        public
+    {}
+
+    //
+    // Implementation of IVotingController
+    //
+
+    function onAddProposal(bytes32 /*proposalId*/, address /*initiator*/, address /*token*/)
+        public
+        constant
+        returns (bool)
+    {
+        return true;
+    }
+
+    function onChangeVotingController(address /*sender*/, IVotingController /*newController*/)
+        public
+        constant
+        returns (bool)
+    {
+        return true;
+    }
+
+    //
+    // Implementation of IContractId
+    //
+
+    function contractId() public pure returns (bytes32 id, uint256 version) {
+        return (VOTING_CONTROLLER, 0);
+    }
+}
